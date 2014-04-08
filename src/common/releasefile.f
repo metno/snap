9cd52040b658b85a02fbed32d63b5e1d631d974e
Heiko Klein <heikok@met.no>
2014-04-08 12:50:35 +0000

    extended snap to run with several release-heights (~10) and many release time steps (~500) release input from separate file in space separated form

---
+++ src/common/releasefile.f
@@ -0,0 +1,149 @@
+      subroutine  releasefile(filename)
+c reading of input-files with hourly data (hours since run-start)
+c comment-rows start with #
+c hour height[upper_in_m] component release[kg/s]
+
+c data is read to
+c  irelhour
+c  relbqsec(time, comp, height)
+c
+c for each release-step, rellower/relupper/relradius are copied from (1,x)
+      implicit none
+
+      include 'snapdim.inc'
+      include 'snappar.inc'
+
+c input
+      character*72 filename
+      character*256 cinput
+c
+      logical debugrelfile
+      integer ifd, ios, iend, iexit, nlines
+      integer i,j
+      integer hour, height, lasthour
+      integer ihour, iheight, icmp
+      real    rel_s
+      character*32 comp
+      debugrelfile = .false.
+      iexit = 0
+
+      write(*,*) 'reading release from: ', filename
+      if (debugrelfile) then
+        write(*,*) 'ncomp, nrelheight', ncomp, nrelheight
+      end if
+
+      ifd=8
+      open(ifd,file=filename,
+     +           access='sequential',form='formatted',
+     +           status='old',iostat=ios)
+      if(ios.ne.0) then
+        write(6,*) 'Open Error: ',filename(1:len(filename,1))
+        stop 1
+      endif
+
+c header row
+      nlines=0
+      iend=0
+      lasthour = -1
+      ihour = 0
+      do while (iend.eq.0)
+        nlines=nlines+1
+        read(ifd,fmt='(a)',err=11) cinput
+        if (debugrelfile) write(*,*) 'cinput (',nlines,'):',cinput
+        if (cinput .eq. "end") goto 18
+        if (cinput(1:1) .ne. '*') then
+          read(cinput, *, err=12) hour, height, comp, rel_s
+          if (lasthour.eq.-1 .and. hour.ne.0) then
+            write(*,*) 'first hour must be 0'
+            goto 12
+          end if
+          if (hour .lt. lasthour) then
+            write(*,*) 'hour must increase monotonic: ',
+     +                   hour, ' < ', lasthour
+            goto 12
+          end if
+          if (hour .gt. lasthour) then
+c add new release timestep
+            lasthour = hour
+            ihour = ihour + 1
+            if (ihour .gt. mtprof) then
+              write(*,*) 'to many release timesteps, increase mtprof'
+              goto 12
+            end if
+            irelhour(ihour) = hour
+c make sure all initial release are undefined
+            do i=1,mcomp
+              do j=1,mrelheight
+                relbqsec(ihour,i,j)= -1.
+              end do
+            end do
+          end if
+c find the component
+          icmp = 0
+          do i=1,ncomp
+            if(comp.eq.component(i)) icmp=i
+          end do
+          if (icmp .eq. 0) then
+            write(*,*) 'unknown component: ',comp
+            goto 12
+          endif
+c find the height
+          iheight = 0
+          do i=1,nrelheight
+            if(height .eq. rellower(1,i)) iheight = i
+          end do
+          if (iheight .eq. 0) then
+            write(*,*) 'unkown lower height: ', height
+            goto 12
+          end if
+c save the release
+          relbqsec(ihour, icmp, iheight) = rel_s
+c end ifnot comment '*'
+        end if
+      end do
+      goto 18
+c
+   11 write(6,*) 'ERROR reading file: ',filename(1:len(filename,1))
+      write(6,*) 'At line no. ',nlines
+      iexit=2
+      goto 18
+c
+   12 write(6,*) 'ERROR reading file: ',filename(1:len(filename,1))
+      write(6,*) 'At line no. ',nlines,' :'
+      write(6,*)  cinput
+      iexit=2
+      goto 18
+
+   18 close(ifd)
+      ntprof = ihour
+      write (*,*) 'finished reading: ', ntprof, ' timesteps'
+c theoretically possible to add time-varying heights/radiuses
+c but not supported by input file format yet
+      do ihour=2,ntprof
+        do iheight=1,nrelheight
+          rellower(ihour,iheight) = rellower(1,iheight)
+          relupper(ihour,iheight) = relupper(1,iheight)
+          relradius(ihour,iheight) = relradius(1,iheight)
+        end do
+      end do
+
+c sanity check of relbqsec
+      do ihour=1,ntprof
+        do icmp=1,ncomp
+          do iheight=1,nrelheight
+            if (relbqsec(ihour,icmp,iheight).lt.0) then
+              relbqsec(ihour,icmp,iheight) = 0
+              write(*,*) 'no release for (',component(icmp),',',
+     c               rellower(ihour,iheight),'m,',irelhour(ihour),'h)'
+            end if
+          end do
+        end do
+      end do
+
+      if (iexit.ne.0) then
+        write(*,*) 'aborted in releasefile.f'
+        call exit(1)
+      end if
+
+
+      end subroutine  releasefile
