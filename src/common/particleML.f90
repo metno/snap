20386e980e6909d252c12bbd26d1977ca564c6ee
Heiko Klein <heikok@met.no>
2015-08-04 14:02:03 +0000

    using position-interpolation before loop store short-term parameters in pextra, not pdata

--- src/common/particleML.f90
+++ src/common/particleML.f90
@@ -18,11 +18,11 @@ module particleML
        REAL    :: z     !- sigma/eta position (vertical)
        REAL    :: tbl   !- sigma/eta at top of boundary layer
        REAL    :: rad   !- radioactive content (Bq)
-       ! to be removed
        REAL    :: hbl
-       REAL    :: rmx
-       REAL    :: rmy ! map ration in y direction
-       REAL    :: prc ! precipition intensity (mm/hour)
+       ! to be removed
+!       REAL    :: rmx
+!       REAL    :: rmy ! map ration in y direction
+!       REAL    :: prc ! precipition intensity (mm/hour)
        REAL    :: grv ! gravity in m/s (fixed or computed)
        INTEGER*2 :: ageInSteps ! age of particle since construction
        LOGICAL :: active ! inside/outside domain
@@ -32,11 +32,9 @@ module particleML
     TYPE extraParticle
        REAL    :: u ! u-speed
        REAL    :: v ! v-speed
-       REAL    :: hbl ! height of boundary layer
        REAL    :: rmx ! map ratio in x direction
        REAL    :: rmy ! map ration in y direction
-       REAL    :: precip ! precipition intensity (mm/hour)
-       REAL    :: grav ! gravity in m/s (fixed or computed)
+       REAL    :: prc ! precipition intensity (mm/hour)
     END TYPE extraParticle
 
 ! the actual particle storage, will be allocated in allocateFields.F
