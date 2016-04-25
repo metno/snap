303971834c880e108739b2a417d466df318332d0
Heiko Klein <heikok@met.no>
2016-04-25 14:46:18 +0000

    implement form and javascript handlers

--- utils/SnapPy/__main__.py
+++ utils/SnapPy/__main__.py
@@ -3,6 +3,7 @@ from Resources import Resources
 from MainBrowserWindow import MainBrowserWindow
 from PyQt5 import QtCore, QtGui, QtWidgets, QtWebKit, QtWebKitWidgets
 
+
 def snappy(argv) :
     res = Resources()
     app = QtWidgets.QApplication(argv)
@@ -11,6 +12,8 @@ def snappy(argv) :
     main.setWindowIcon(QtGui.QIcon(res.getIconPath()))
     main.show()
     main.set_html(res.getStartScreen())
+    main.set_form_handler(MainBrowserWindow._default_form_handler)
+    main.evaluate_javaScript(str.format('updateSnapLog("{0}");',"working..."));
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
