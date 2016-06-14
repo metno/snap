f34466b2c41e77697f9cac6935a41919bd022fe9
Heiko Klein <heikok@met.no>
2016-06-14 11:34:04 +0000

    release v1.1

--- snapPkg.sh
+++ snapPkg.sh
@@ -1,11 +1,13 @@
-VERSION=1.0
+VERSION=1.1
 cd src
-make dist
+VERSION=$VERSION make dist
 cd ..
 tar xvfz snap-${VERSION}.tgz
-mv snap-${VERSION}.tgz bsnap_1.0.orig.tar.gz
+mv snap-${VERSION}.tgz bsnap_${VERSION}.orig.tar.gz
 cd snap-${VERSION}
 cp -r ../debian .
+dch -v 1.1-1 "new release"
+dch -r ""
 debuild -us -uc -sa
 
 
