16a4f250cc904960bf50df559be173d62fc5130c
Heiko Klein <heikok@met.no>
2016-07-04 08:53:22 +0000

    fixing segmentation-fault in trajectories with netcdf-files

--- src/common/init_random_seed.f
+++ src/common/init_random_seed.f
@@ -8,5 +8,5 @@
             seed = 3 + 37 * (/ (i - 1, i = 1, n) /)
             CALL RANDOM_SEED(PUT = seed)
 
-            DEALLOCATE(seed)
+!  DEALLOCATE(seed), not called unsure if this doesn't break later calls to random-generator
       END SUBROUTINE
