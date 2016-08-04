30571f638d73e44d931752bfefdea6a615dc3a13
Heiko Klein <heikok@met.no>
2016-08-04 14:35:09 +0000

    starting with eemep support in snapPy, tab-based

---
+++ utils/SnapPy/Snappy/__init__.py
@@ -0,0 +1,4 @@
+from Snappy.Resources import Resources
+from Snappy.BrowserWidget import BrowserWidget
+from Snappy.MainBrowserWindow import MainBrowserWindow
+from Snappy.SnapController import SnapController
