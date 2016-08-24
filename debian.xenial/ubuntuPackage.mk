7b5348534fb9b7f4cbe927bbb809420888b99591
Heiko Klein <heiko.klein@met.no>
2016-08-24 16:08:55 +0200

    fixing packing for xenial

---
+++ debian.xenial/ubuntuPackage.mk
@@ -0,0 +1,49 @@
+# link this to current.mk and it will be used in the Makefiles in subdirectories
+# includefile contains Compiler definitions etc.
+
+F77 = gfortran
+CXX = g++
+CC  = gcc
+
+F77FLAGS=-O2 -g -mavx -cpp -ftree-vectorize -fno-math-errno
+CXXFLAGS=-O2 -mavx -ftree-vectorize -fno-math-errno
+CCFLAGS=-O2 -mavx -ftree-vectorize -fno-math-errno
+
+LDFLAGS=
+
+# NCDIR required even if /usr or /usr/local
+NCDIR = /usr
+
+
+MIINC = -I/usr/include
+# uncomment MILIB if felt-support not required
+MILIB = -L/usr/lib -lmi
+EXLIBS = -lpthread -ldl
+
+DRHOOKINC = -I../../utils/drhook_CY31R2.032
+DRHOOKLIB = -L../../utils/drhook_CY31R2.032 -ldrhook -lmpi_serial
+
+##########################################################
+
+BINDIR=../debian/bsnap/usr/bin/
+
+INCLUDES = -I. $(MIINC)
+
+
+ifdef NCDIR
+BLIBS += -L$(NCDIR)/lib -Wl,-rpath,$(NCDIR)/lib
+INCLUDES += -I$(NCDIR)/include
+endif
+
+LIBS= $(MILIB) $(EXLIBS)
+BLIBS += $(MILIB) -lnetcdff
+
+ifdef DR_HOOK
+$(info DR_HOOK defined)
+F77FLAGS += -DDRHOOK $(DRHOOKINC)
+BLIBS += $(DRHOOKLIB)
+endif
+
+# clear out all suffixes
+.SUFFIXES:
+
