645b04db4a6ec725688389264cbdc7a5847c63f8
Heiko Klein <heiko.klein@met.no>
2017-08-01 10:43:17 +0200

    adding ppi_direct to work on lustre directly

--- utils/SnapPy/METNO/HPC.py
+++ utils/SnapPy/METNO/HPC.py
@@ -248,6 +248,9 @@ class HPC():
         elif (name == "alvin"):
             from . import Alvin
             return Alvin.Alvin()
+        elif (name == "ppi_direct"):
+            from . import PPI_Direct
+            return PPI_Direct.PPI_Direct()
         else:
             raise NotImplementedError("no HPC named '{}'".format(name))
         return
