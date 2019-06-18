2cbb6406e899bebfc84d9e8213304e956fd0bfe3
Magnus Ulimoen <magnusu@met.no>
2019-06-18 10:12:56 +0200

    lint snapfldml

--- src/common/snapfldML.f90
+++ src/common/snapfldML.f90
@@ -1,81 +1,123 @@
 ! SNAP: Servere Nuclear Accident Programme
 ! Copyright (C) 1992-2017   Norwegian Meteorological Institute
-! 
-! This file is part of SNAP. SNAP is free software: you can 
-! redistribute it and/or modify it under the terms of the 
-! GNU General Public License as published by the 
+!
+! This file is part of SNAP. SNAP is free software: you can
+! redistribute it and/or modify it under the terms of the
+! GNU General Public License as published by the
 ! Free Software Foundation, either version 3 of the License, or
 ! (at your option) any later version.
-! 
+!
 ! This program is distributed in the hope that it will be useful,
 ! but WITHOUT ANY WARRANTY; without even the implied warranty of
 ! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ! GNU General Public License for more details.
-! 
+!
 ! You should have received a copy of the GNU General Public License
 ! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 !
 module snapfldML
-    USE iso_fortran_env, only: real32, real64
-    implicit none
-    private
-!
-!..include file  -  common for fields
-!
-!
-!..1:      input time step 1
-!..2:      input time step 2
-!..u,v:    horizontal wind components
-!..w:      sigma_dot
-!..t:      potential temperature (level 1, surface: abs. temp. 2m)
-!..ps:     surface pressure
-!..bl:     boudary layer top in sigma coordinate
-!..hbl:    boudary layer depth in meters
-!..xm:     map ratio in x direction
-!..ym:     map ratio in y direction
-!..garea:  grid square area (m**2)
-!..field*: work arrays
-!..pmsl:   mean sea level pressure (mslp, not used in computations)
-!	   for output (possible if nxad=nx and nyad=ny)
-!..precip: hourly precipitation intensity (mm/hour)
-!..nprecip: no. of steps stored
-!..iprecip: the current precipitation field
-!..enspos: the ensemble-member to read met-data from
-!
-      REAL(kind=real32), DIMENSION(:,:,:), allocatable, save, public :: &
-        u1, v1, w1, t1, hlevel1, hlayer1, &
-        u2, v2, w2, t2, hlevel2, hlayer2, &
-        precip
+  USE iso_fortran_env, only: real32, real64
+  implicit none
+  private
 
-      REAL(kind=real32), DIMENSION(:,:), allocatable, save, public :: &
-        ps1, bl1, hbl1, &
-        ps2, bl2, hbl2, &
-        xm, ym, garea, field1, field2, field3, &
-        field4, pmsl1, pmsl2
+!>  horizontal wind component (time step 1)
+  real(kind=real32), allocatable, save, public :: u1(:,:,:)
+!>  horizontal wind component (time step 2)
+  real(kind=real32), allocatable, save, public :: u2(:,:,:)
+!>  horizontal wind component (time step 1)
+  real(kind=real32), allocatable, save, public :: v1(:,:,:)
+!>  horizontal wind component (time step 2)
+  real(kind=real32), allocatable, save, public :: v2(:,:,:)
+!>  sigma_dot (time step 1)
+  real(kind=real32), allocatable, save, public :: w1(:,:,:)
+!>  sigma_dot (time step 2)
+  real(kind=real32), allocatable, save, public :: w2(:,:,:)
 
-      integer, save, public :: nprecip,iprecip,enspos
-!
-!
-!..dgarea:  grid square area (m**2) ... double precision
-!..depdry:  dry deposition
-!..depwet:  wet deposition
-!..accdry:  accumulated dry deposition
-!..accwet:  accumulated wet deposition
-!..concacc: accumulated/integrated concentration
-!..avghbl:  average height of boundary layer (accumulation)
-!..avgprec: precipitation  (accumulation)
-!..accprec: accumulation of precipitation from start of run
-!..avgbq1:  average Bq (per square area) in boundary layer (accum.)
-!..avgbq2:  average Bq (per square area) above boundary layer (accum.)
-!..avgbq:   average Bq (per square area) in each layer (accum.)
-!..	    only used if (nxmc=nx, nymc=ny and imodlevel=1)
-!
-      REAL(kind=real64), DIMENSION(:,:), allocatable, save, public :: &
-        dgarea, avghbl, avgprec, accprec
-      REAL(kind=real64), DIMENSION(:,:,:), allocatable, save, public :: &
-        depdry, depwet, accdry, accwet, &
-        concen, concacc, avgbq1, avgbq2
-      REAL(kind=real64), DIMENSION(:,:,:,:), allocatable, save, public :: &
-        avgbq
-!
+!> potential temperature (level 1, surface: abs. temp. 2m) (time step 1)
+  real(kind=real32), allocatable, save, public :: t1(:,:,:)
+!> potential temperature (level 1, surface: abs. temp. 2m) (time step 2)
+  real(kind=real32), allocatable, save, public :: t2(:,:,:)
+
+  real(kind=real32), allocatable, save, public :: hlevel1(:,:,:)
+  real(kind=real32), allocatable, save, public :: hlevel2(:,:,:)
+  real(kind=real32), allocatable, save, public :: hlayer1(:,:,:)
+  real(kind=real32), allocatable, save, public :: hlayer2(:,:,:)
+
+!> hourly precipitation intensity (mm/hour)
+  real(kind=real32), allocatable, save, public :: precip(:,:,:)
+!> no. of steps stored in ::precip
+  integer, save, public :: nprecip
+!> the current precipitation field
+  integer, save, public :: iprecip
+
+!> surface pressure (time step 1)
+  real(kind=real32), allocatable, save, public :: ps1(:,:)
+!> surface pressure (time step 2)
+  real(kind=real32), allocatable, save, public :: ps2(:,:)
+!> mean sea level pressure (mslp, not used in computations)
+!> for output (possible if nxad=nx and nyad=ny) (time step 1)
+  real(kind=real32), allocatable, save, public :: pmsl1(:,:)
+!> mean sea level pressure (mslp, not used in computations)
+!> for output (possible if nxad=nx and nyad=ny) (time step 2)
+  real(kind=real32), allocatable, save, public :: pmsl2(:,:)
+
+!> boundary layer top in sigma coordinate (time step 1)
+  real(kind=real32), allocatable, save, public :: bl1(:,:)
+!> boundary layer top in sigma coordinate (time step 2)
+  real(kind=real32), allocatable, save, public :: bl2(:,:)
+!> boundary layer depth in meters (time step 1)
+  real(kind=real32), allocatable, save, public :: hbl1(:,:)
+!> boundary layer depth in meters (time step 2)
+  real(kind=real32), allocatable, save, public :: hbl2(:,:)
+
+!> map ratio in x direction
+  real(kind=real32), allocatable, save, public :: xm(:,:)
+!> map ratio in y direction
+  real(kind=real32), allocatable, save, public :: ym(:,:)
+!> grid square area (m**2)
+  real(kind=real32), allocatable, save, public :: garea(:,:)
+!> grid square area (m**2) ... double precision
+  real(kind=real64), allocatable, save, public :: dgarea(:,:)
+
+
+!> Work array
+  real(kind=real32), allocatable, save, public :: field1(:,:)
+!> Work array
+  real(kind=real32), allocatable, save, public :: field2(:,:)
+!> Work array
+  real(kind=real32), allocatable, save, public :: field3(:,:)
+!> Work array
+  real(kind=real32), allocatable, save, public :: field4(:,:)
+
+!> the ensemble-member to read met-data from
+  integer, save, public :: enspos
+
+!> average height of boundary layer (accumulation)
+  real(kind=real64), allocatable, save, public :: avghbl(:,:)
+!> precipitation  (accumulation)
+  real(kind=real64), allocatable, save, public :: avgprec(:,:)
+!> accumulation of precipitation from start of run
+  real(kind=real64), allocatable, save, public :: accprec(:,:)
+
+!> dry deposition
+  real(kind=real64), allocatable, save, public :: depdry(:,:,:)
+!> wet deposition
+  real(kind=real64), allocatable, save, public :: depwet(:,:,:)
+
+!> accumulated dry deposition
+  real(kind=real64), allocatable, save, public :: accdry(:,:,:)
+!> accumulated wet deposition
+  real(kind=real64), allocatable, save, public :: accwet(:,:,:)
+!> accumulated/integrated concentration
+  real(kind=real64), allocatable, save, public :: concacc(:,:,:)
+!> average Bq (per square area) in boundary layer (accum.)
+  real(kind=real64), allocatable, save, public :: avgbq1(:,:,:)
+!> average Bq (per square area) above boundary layer (accum.)
+  real(kind=real64), allocatable, save, public :: avgbq2(:,:,:)
+
+  real(kind=real64), allocatable, save, public :: concen(:,:,:)
+!> average Bq (per square area) in each layer (accum.)
+!>
+!> only used if (nxmc=nx, nymc=ny and imodlevel=1)
+  real(kind=real64), allocatable, save, public :: avgbq(:,:,:,:)
 end module snapfldML
