aa7a05d0bab59926fe29c422cea92178efe4a496
Magnus Ulimoen <magnusu@met.no>
2019-06-20 12:09:10 +0200

    lint wetdep

--- src/common/wetdep.f90
+++ src/common/wetdep.f90
@@ -30,29 +30,23 @@ module wetdep
 
   contains
 
+!> Purpose:  Compute wet deposition for each particle and each component
+!>           and store depositions in nearest gridpoint in a field
+!>
+!> Method:   J.Saltbones 1994
 subroutine wetdep1(n,pextra)
-  USE particleML
-  USE snapgrdML
-  USE snapfldML
-  USE snapparML
+  USE particleML, only: pdata, extraParticle
+  USE snapfldML, only: depwet
+  USE snapparML, only: icomp, iruncomp
   USE snaptabML, only: mpretab, premult, pretab
 
-!  Purpose:  Compute wet deposition for each particle and each component
-!            and store depositions in nearest gridpoint in a field
-!  Method:   J.Saltbones 1994
+!> particle loop index, n = 0 means init
+  integer, intent(in) :: n
+  Type(extraParticle), intent(inout) :: pextra
 
-
-  implicit none
-
-
-! particle loop index, n = 0 means init
-  INTEGER, INTENT(IN) :: n
-  TYPE(extraParticle), INTENT(INOUT) :: pextra
   integer :: m,itab,i,j,mm
-  real ::    precint,probab,prand,dep
+  real :: precint,probab,prand,dep
 
-
-!      do n=1,npart // particle loop moved outside subroutine
   m= icomp(n)
   if(kwetdep(m) == 1 .AND. pextra%prc > 0.0) then
   !..find particles with wet deposition and
@@ -75,83 +69,62 @@ subroutine wetdep1(n,pextra)
       depwet(i,j,mm)=depwet(i,j,mm)+dble(dep)
     end if
   end if
-!      end do
 
   return
 end subroutine wetdep1
 
+
+!> Purpose:  Compute wet deposition for each particle and each component
+!>           and store depositions in nearest gridpoint in a field
+!>
+!> Method:   J.Bartnicki 2003
 subroutine wetdep2(tstep,np,pextra)
-
-!  Purpose:  Compute wet deposition for each particle and each component
-!            and store depositions in nearest gridpoint in a field
-!  Method:   J.Bartnicki 2003
-
 ! ... 23.04.12 - gas, particle 0.1<d<10, particle d>10 - J. Bartnicki|
 ! ... 12.12.13 - gas 'particle size' changed to 0.05um - H. Klein
-  use particleML
-  USE snapgrdML
-  USE snapfldML
-  USE snapparML
-  USE snaptabML
+  USE particleML, only: pdata, extraParticle
+  USE snapfldML, only: depwet
+  USE snapparML, only: ncomp, icomp, iruncomp
   USE snapdimML, only: mdefcomp
   USE snapdebug, only: iulog
   USE vgravtablesML, only: radiusmym
-  implicit none
 
-  real, INTENT(IN) ::    tstep
+  real, intent(in) ::    tstep
+!> particle loop index, np = 0 means init
+  integer, intent(in) :: np
+  type(extraParticle), intent(in) :: pextra
 
-  real ::    a0,a1,a2,b0,b1,b2,b3
+  real, parameter :: a0=8.4e-5
+  real, parameter :: a1=2.7e-4
+  real, parameter :: a2=-3.618e-6
+  real, parameter :: b0=-0.1483
+  real, parameter :: b1=0.3220133
+  real, parameter :: b2=-3.0062e-2
+  real, parameter :: b3=9.34458e-4
 
-  parameter (a0=8.4e-5)
-  parameter (a1=2.7e-4)
-  parameter (a2=-3.618e-6)
-  parameter (b0=-0.1483)
-  parameter (b1=0.3220133)
-  parameter (b2=-3.0062e-2)
-  parameter (b3=9.34458e-4)
-
-! particle loop index, np = 0 means init
-  INTEGER, INTENT(IN) :: np
-  TYPE(extraParticle), INTENT(IN) :: pextra
   integer :: m,n,i,j,mm
-  real ::    precint,deprate,dep,q,rkw
-  real ::    depconst(mdefcomp)
+  real :: precint,deprate,dep,q,rkw
+  real, save :: depconst(mdefcomp)
 
-!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   real :: ratdep(mdefcomp)
-!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-!################################################################
-  integer :: numdep
-  real :: depmin,depmax,ratmin,ratmax,premin,premax,rm
-  double precision :: totinp,depsum,totsum
-!################################################################
-
-  save depconst
+  real :: rm
 
 ! initalization
   if(np == 0) then
-  
+
     do m=1,ncomp
       rm=radiusmym(m)
       depconst(m)=b0 + b1*rm + b2*rm*rm + b3*rm*rm*rm
-    !################################################################
       write(iulog,*) 'WETDEP2 m,r,depconst(m): ',m,rm,depconst(m)
-    !################################################################
     end do
-  
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
     write(iulog,*) '-------------------------------------------------'
     write(iulog,*) 'WETDEP2 PREPARE .... q,deprate(1:ncomp):'
-  !##############################################
-  ! c	dep=radiusmym(1)
-  ! c	radiusmym(1)=1.0
-  !##############################################
+
     do n=1,200
       q=float(n)*0.1
       do m=1,ncomp
-      
+
       ! b... 25.04.12 wet deposition for convective and gases
-      
         if(radiusmym(m) > 0.05 .AND. radiusmym(m) <= 1.4) then
           rkw= a0*q**0.79
         endif
@@ -161,10 +134,10 @@ subroutine wetdep2(tstep,np,pextra)
         if(radiusmym(m) > 10.0) then
           rkw= a1*q + a2*q*q
         endif
-        if(q > 7.0) then	! convective
+        if(q > 7.0) then ! convective
           rkw=3.36e-4*q**0.79
         endif
-        if(radiusmym(m) <= 0.05) then	! gas
+        if(radiusmym(m) <= 0.05) then ! gas
           rkw=1.12e-4*q**0.79
         endif
         deprate= 1.0 - exp(-tstep*rkw)
@@ -173,43 +146,20 @@ subroutine wetdep2(tstep,np,pextra)
       write(iulog,1010) q,(ratdep(m),m=1,ncomp)
       1010 format(1x,f5.1,':',12f7.4)
     end do
-  !##############################################
-  ! c	radiusmym(1)=dep
-  !##############################################
     write(iulog,*) '-------------------------------------------------'
-  !++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  ! end init
+
     return
   end if
 
-!################################################################
-  numdep=0
-  premin=+1.e+38
-  premax=-1.e+38
-  ratmin=+1.e+38
-  ratmax=-1.e+38
-  depmin=+1.e+38
-  depmax=-1.e+38
-  totinp=0.0d0
-  depsum=0.0d0
-  totsum=0.0d0
-!################################################################
-
-!      do np=1,npart // particle loop moved out
-!################################################################
-  totinp=totinp+dble(pdata(np)%rad)
-!################################################################
   m= icomp(np)
-!	if(kwetdep(m).eq.1 .and. pextra%prc.gt.0.0) then
   if(kwetdep(m) == 1 .AND. pextra%prc > 0.0 &
-   .AND. pdata(np)%z > 0.67) then
+      .AND. pdata(np)%z > 0.67) then
   !..find particles with wet deposition and
   !..reset precipitation to zero if not wet deposition
     precint=pextra%prc
     q=precint
-  
+
   ! b... 25.04.12 wet deposition for convective and gases
-  
     if(radiusmym(m) > 0.05 .AND. radiusmym(m) <= 1.4) then
       rkw= a0*q**0.79
     endif
@@ -219,10 +169,10 @@ subroutine wetdep2(tstep,np,pextra)
     if(radiusmym(m) > 10.0) then
       rkw= a1*q + a2*q*q
     endif
-    if(q > 7.0) then	! convective
+    if(q > 7.0) then ! convective
       rkw=3.36e-4*q**0.79
     endif
-    if(radiusmym(m) <= 0.1) then	! gas
+    if(radiusmym(m) <= 0.1) then ! gas
       rkw=1.12e-4*q**0.79
     endif
     deprate= 1.0 - exp(-tstep*rkw)
@@ -234,34 +184,7 @@ subroutine wetdep2(tstep,np,pextra)
     mm=iruncomp(m)
   ! omp atomic
     depwet(i,j,mm)=depwet(i,j,mm)+dble(dep)
-  !################################################################
-    if(premin > precint) premin=precint
-    if(premax < precint) premax=precint
-    if(ratmin > deprate) ratmin=deprate
-    if(ratmax < deprate) ratmax=deprate
-    if(depmin > dep) depmin=dep
-    if(depmax < dep) depmax=dep
-    depsum=depsum+dble(dep)
-    numdep=numdep+1
-  !################################################################
   end if
-!################################################################
-  totsum=totsum+dble(pdata(np)%rad)
-!################################################################
-!      end do
-
-!################################################################
-!      write(88,*) 'WETDEP2 numdep,npart:  ',numdep,npart
-!      write(88,*) 'WETDEP2 totinp:        ',totinp
-!      write(88,*) 'WETDEP2 totsum,depsum: ',totsum,depsum
-!      if(premin.le.premax)
-!     +   write(88,*) 'WETDEP2 premin,premax: ',premin,premax
-!      if(ratmin.le.ratmax)
-!     +   write(88,*) 'WETDEP2 ratmin,ratmax: ',ratmin,ratmax
-!      if(depmin.le.depmax)
-!     +   write(88,*) 'WETDEP2 depmin,depmax: ',depmin,depmax
-!      write(88,*) '---------------------------------------'
-!################################################################
   return
 end subroutine wetdep2
 end module wetdep
