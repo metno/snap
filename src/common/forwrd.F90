5244d09a03a10e183237cf8580dd76de849ae8dc
Magnus Ulimoen <magnusu@met.no>
2019-06-21 11:41:04 +0200

    lint forwrd

--- src/common/forwrd.F90
+++ src/common/forwrd.F90
@@ -20,7 +20,7 @@ module forwrdML
   private
 
 #if defined(TRAJ)
-!... calculation of distance and speed
+!> calculation of distance and speed
   real, save, public :: speed
 #endif
 
@@ -28,66 +28,64 @@ module forwrdML
 
   contains
 
-subroutine forwrd(tf1,tf2,tnow,tstep,np,pextra)
-
-!  Purpose:  Move all particles one timestep forward
-
-!  Notes:
-!    - sigma levels (norlam) or eta levels (hirlam,...)
-!      defined by alevel and blevel
-!    - horizontal wind components in unit m/s
-!    - vertical   wind component  in unit vlevel/second
-!      (vlevel = sigma or eta(alevel,blevel))
-!    - all wind components in non-staggered horizontal grid
-!      and in the same levels
-!    - lower model level is level 2
-
-!  Input:
-!       tf1:   time in seconds for field set 1 (e.g. 0.)
-!       tf2:   time in seconds for field set 2 (e.g. 21600, if 6 hours)
-!       tnow:  time in seconds for current paricle positions
-!       tstep: timestep in seconds
+!> Purpose:  Move all particles one timestep forward
+!>
+!> Notes:
+!>   - sigma levels (norlam) or eta levels (hirlam,...)
+!>     defined by alevel and blevel
+!>   - horizontal wind components in unit m/s
+!>   - vertical   wind component  in unit vlevel/second
+!>     (vlevel = sigma or eta(alevel,blevel))
+!>   - all wind components in non-staggered horizontal grid
+!>     and in the same levels
+!>   - lower model level is level 2
+subroutine forwrd(tf1, tf2, tnow, tstep, np, pextra)
   USE iso_fortran_env, only: real64
-  USE particleML
-  USE snapgrdML
-  implicit none
+  USE particleML, only: pdata, particle, extraParticle
+  USE snapgrdML, only: vlevel
+!> time in seconds for field set 1 (e.g. 0.)
+  real, intent(in) :: tf1
+!> time in seconds for field set 2 (e.g. 21600, if 6 hours)
+  real, intent(in) :: tf2
+!> time in seconds for current paricle positions
+  real, intent(in) :: tnow
+!> timestep in seconds
+  real, intent(in) :: tstep
+!> particle loop index, np = 0 is initalization
+  integer, intent(in) :: np
 
-  real, INTENT(IN) :: tf1,tf2,tnow,tstep
+  type(extraParticle), intent(inout) :: pextra
 
-! particle loop index, np = 0 is initalization
-  INTEGER, INTENT(IN) :: np
-  TYPE(extraParticle), INTENT(INOUT) :: pextra
-
-  TYPE(particle) :: nparticle
-  REAL(real64) :: dx1, dy1, dz1, u, v
+  real(real64) :: dx1, dy1, dz1, u, v
 #if defined(PETTERSEN)
-  REAL(real64) :: dx2, dy2, dz2
-  REAL :: vmin, vmax
+  type(particle) :: nparticle
+  real(real64) :: dx2, dy2, dz2
+  real :: vmin, vmax
 #endif
 
 
   if (np == 0) then
   ! init
-    call forwrd_dx(tf1,tf2,tnow,tstep,np,nparticle,dx1,dy1,dz1,u,v)
+    call forwrd_dx(tf1,tf2,tnow,tstep,np,dx1,dy1,dz1,u,v)
     return
   endif
 
-  nparticle = pdata(np)
-  call forwrd_dx(tf1,tf2,tnow,tstep,np,nparticle, dx1, dy1, dz1, &
-  u, v)
+  call forwrd_dx(tf1,tf2,tnow,tstep,np, dx1, dy1, dz1, &
+      u, v)
 !..store u,v for rwalk
   pextra%u=u
   pextra%v=v
 #if defined(TRAJ)
-  speed=sqrt(u*u+v*v)
+  speed=hypot(u, v)
 #endif
 #if defined(PETTERSEN)
+  nparticle = pdata(np)
   nparticle%x=nparticle%x+dx1*pextra%rmx
   nparticle%y=nparticle%y+dy1*pextra%rmy
   nparticle%z=nparticle%z+dz1
   if (nparticle%x < 1. .OR. nparticle%y < 1. .OR. &
-  nparticle%x > nx .OR. nparticle%y > ny) then
-    nparticle%active = .FALSE. 
+      nparticle%x > nx .OR. nparticle%y > ny) then
+    nparticle%active = .FALSE.
     pdata(np) = nparticle
   else
     vmin=vlevel(nk)
@@ -99,7 +97,7 @@ subroutine forwrd(tf1,tf2,tnow,tstep,np,pextra)
     end if
 
     call forwrd_dx(tf1,tf2,tnow+tstep,tstep, &
-    np,nparticle, dx2, dy2, dz2, u, v)
+        np, dx2, dy2, dz2, u, v)
     pdata(np)%x=pdata(np)%x+.5*(dx1 + dx2)*pextra%rmx
     pdata(np)%y=pdata(np)%y+.5*(dy1 + dy2)*pextra%rmy
     pdata(np)%z=pdata(np)%z+.5*(dz1 + dz2)
@@ -114,65 +112,82 @@ subroutine forwrd(tf1,tf2,tnow,tstep,np,pextra)
   return
 end subroutine forwrd
 
-subroutine forwrd_dx(tf1,tf2,tnow,tstep,np,nparticle, &
-  delx, dely, delz, u, v)
+
+!> Purpose:  calculate dx,dy,dz forward movement of particle at pos np
+!>
+!> Notes:
+!>   - sigma levels (norlam) or eta levels (hirlam,...)
+!>     defined by alevel and blevel
+!>   - horizontal wind components in unit m/s
+!>   - vertical   wind component  in unit vlevel/second
+!>     (vlevel = sigma or eta(alevel,blevel))
+!>   - all wind components in non-staggered horizontal grid
+!>     and in the same levels
+!>   - lower model level is level 2
+subroutine forwrd_dx(tf1, tf2, tnow, tstep, np, &
+    delx, dely, delz, u, v)
   USE iso_fortran_env, only: real64
-  USE particleML
-  USE snapgrdML
-  USE snapfldML
-  USE snapparML
-  USE snaptabML
+  USE particleML, only: pdata, particle
+  USE snapgrdML, only: gparam, vlevel, vhalf, alevel, ahalf, blevel, bhalf, &
+      ivlayer, ivlevel
+  USE snapfldML, only: u1, u2, v1, v2, w1, w2, t1, t2, ps1, ps2
+  USE snapparML, only: ncomp, idefcomp, kgravity, compname, icomp, gravityms
+  USE snaptabML, only: cp, g, pmult, r, pitab
   USE vgravtablesML, only: vgravtables, vgtable, pbasevg, tbasevg, pincrvg, tincrvg
   USE snapdimML, only: nk
   USE snapdebug, only: iulog
 
-!  Purpose:  calculate dx,dy,dz forward movement of particle at pos np
+!> time in seconds for field set 1 (e.g. 0.)
+  real, intent(in) :: tf1
+!> time in seconds for field set 2 (e.g. 21600, if 6 hours)
+  real, intent(in) :: tf2
+!> time in seconds for current paricle positions
+  real, intent(in) :: tnow
+!> timestep in seconds
+  real, intent(in) :: tstep
+!> particle loop index, np = 0 is initalisation
+  integer, intent(in) :: np
 
-!  Notes:
-!    - sigma levels (norlam) or eta levels (hirlam,...)
-!      defined by alevel and blevel
-!    - horizontal wind components in unit m/s
-!    - vertical   wind component  in unit vlevel/second
-!      (vlevel = sigma or eta(alevel,blevel))
-!    - all wind components in non-staggered horizontal grid
-!      and in the same levels
-!    - lower model level is level 2
-
-!  Input:
-!       tf1:   time in seconds for field set 1 (e.g. 0.)
-!       tf2:   time in seconds for field set 2 (e.g. 21600, if 6 hours)
-!       tnow:  time in seconds for current paricle positions
-!       tstep: timestep in seconds
-!  Output:
-!       delx, dely, delz: offset in x,y,z; x/y without mapfactor
-!       u, v: wind-speed in x/y
-  implicit none
-  real, INTENT(IN) :: tf1,tf2,tnow,tstep
-  INTEGER, INTENT(IN) :: np
-  TYPE(particle), INTENT(IN) :: nparticle
-  REAL(real64), INTENT(OUT) :: delx, dely, delz, u, v
+!> delx: offset in x; without mapfactor
+  real(real64), intent(out) :: delx
+!> dely: offset in y; without mapfactor
+  real(real64), intent(out) :: dely
+!> delz: offset in z
+  real(real64), intent(out) :: delz
+!> wind-speed in x
+  real(real64), intent(out) :: u
+!> wind-speed in y
+  real(real64), intent(out) :: v
 
   integer :: i,j,m,ilvl,k1,k2,itab,kt1,kt2,ip,it
   real(real64) ::    dt,rt1,rt2,dx,dy,c1,c2,c3,c4,vlvl
   real(real64) ::    dz1,dz2,uk1,uk2,vk1,vk2,wk1,wk2,w
   real(real64) ::    th,tk1,tk2,ps,p,pi,t,gravity,grav1,grav2,pvg,tvg
   real(real64) ::    pi1,pi2,dz,deta,wg,rtab
+  logical :: compute_grav_table
 ! c
 
-  real(real64), save :: cpinv, rcpinv, ginv, vmax, dxgrid,dygrid
+  real(real64), parameter :: ginv = 1.0/g
+  real(real64), parameter :: cpinv = 1.0/cp
+  real(real64), parameter :: rcpinv = cp/r
+  real(real64), save :: vmax, dxgrid,dygrid
 
 
 
 ! initialization
   if (np == 0) then
-    j=0
-    do i=1,ncomp
+    compute_grav_table = .false.
+    grav_do: do i=1,ncomp
       m= idefcomp(i)
-      if(kgravity(m) > 1) j=j+1
-    end do
-    if (j > 0) then
+      if(kgravity(m) > 1) then
+        compute_grav_table = .true.
+        exit grav_do
+      endif
+    end do grav_do
+
+    if (compute_grav_table) then
       write(iulog,*) 'Computing gravity tables...'
-      call vgravtables
+      call vgravtables()
       write(iulog,*) 'Surface gravity (1000hPa, 300K):'
       it = (300-tbasevg)/tincrvg
       ip = (1000-pbasevg)/pincrvg
@@ -183,9 +198,6 @@ subroutine forwrd_dx(tf1,tf2,tnow,tstep,np,nparticle, &
     else
       write(iulog,*) 'Computation of gravity tables not needed'
     end if
-    ginv= 1./g
-    cpinv= 1./cp
-    rcpinv=cp/r
     dxgrid=gparam(7)
     dygrid=gparam(8)
     vmax=vlevel( 1)
@@ -201,9 +213,7 @@ subroutine forwrd_dx(tf1,tf2,tnow,tstep,np,nparticle, &
   rt2=(tnow-tf1)/(tf2-tf1)
 
 
-!      do np=1,npart // particle loop moved out (snap.F)
-
-!..for horizotal interpolations
+!..for horizontal interpolations
   i=pdata(np)%x
   j=pdata(np)%y
   dx=pdata(np)%x-i
@@ -225,58 +235,58 @@ subroutine forwrd_dx(tf1,tf2,tnow,tstep,np,nparticle, &
 
 !..u
   uk1= rt1*(c1*u1(i,j,k1)  +c2*u1(i+1,j,k1) &
-  +c3*u1(i,j+1,k1)+c4*u1(i+1,j+1,k1)) &
-  +rt2*(c1*u2(i,j,k1)  +c2*u2(i+1,j,k1) &
-  +c3*u2(i,j+1,k1)+c4*u2(i+1,j+1,k1))
+      +c3*u1(i,j+1,k1)+c4*u1(i+1,j+1,k1)) &
+      +rt2*(c1*u2(i,j,k1)  +c2*u2(i+1,j,k1) &
+      +c3*u2(i,j+1,k1)+c4*u2(i+1,j+1,k1))
   uk2= rt1*(c1*u1(i,j,k2)  +c2*u1(i+1,j,k2) &
-  +c3*u1(i,j+1,k2)+c4*u1(i+1,j+1,k2)) &
-  +rt2*(c1*u2(i,j,k2)  +c2*u2(i+1,j,k2) &
-  +c3*u2(i,j+1,k2)+c4*u2(i+1,j+1,k2))
+      +c3*u1(i,j+1,k2)+c4*u1(i+1,j+1,k2)) &
+      +rt2*(c1*u2(i,j,k2)  +c2*u2(i+1,j,k2) &
+      +c3*u2(i,j+1,k2)+c4*u2(i+1,j+1,k2))
   u=uk1*dz1+uk2*dz2
 !..v
   vk1= rt1*(c1*v1(i,j,k1)  +c2*v1(i+1,j,k1) &
-  +c3*v1(i,j+1,k1)+c4*v1(i+1,j+1,k1)) &
-  +rt2*(c1*v2(i,j,k1)  +c2*v2(i+1,j,k1) &
-  +c3*v2(i,j+1,k1)+c4*v2(i+1,j+1,k1))
+      +c3*v1(i,j+1,k1)+c4*v1(i+1,j+1,k1)) &
+      +rt2*(c1*v2(i,j,k1)  +c2*v2(i+1,j,k1) &
+      +c3*v2(i,j+1,k1)+c4*v2(i+1,j+1,k1))
   vk2= rt1*(c1*v1(i,j,k2)  +c2*v1(i+1,j,k2) &
-  +c3*v1(i,j+1,k2)+c4*v1(i+1,j+1,k2)) &
-  +rt2*(c1*v2(i,j,k2)  +c2*v2(i+1,j,k2) &
-  +c3*v2(i,j+1,k2)+c4*v2(i+1,j+1,k2))
+      +c3*v1(i,j+1,k2)+c4*v1(i+1,j+1,k2)) &
+      +rt2*(c1*v2(i,j,k2)  +c2*v2(i+1,j,k2) &
+      +c3*v2(i,j+1,k2)+c4*v2(i+1,j+1,k2))
   v=vk1*dz1+vk2*dz2
 !..w
   wk1= rt1*(c1*w1(i,j,k1)  +c2*w1(i+1,j,k1) &
-  +c3*w1(i,j+1,k1)+c4*w1(i+1,j+1,k1)) &
-  +rt2*(c1*w2(i,j,k1)  +c2*w2(i+1,j,k1) &
-  +c3*w2(i,j+1,k1)+c4*w2(i+1,j+1,k1))
+      +c3*w1(i,j+1,k1)+c4*w1(i+1,j+1,k1)) &
+      +rt2*(c1*w2(i,j,k1)  +c2*w2(i+1,j,k1) &
+      +c3*w2(i,j+1,k1)+c4*w2(i+1,j+1,k1))
   wk2= rt1*(c1*w1(i,j,k2)  +c2*w1(i+1,j,k2) &
-  +c3*w1(i,j+1,k2)+c4*w1(i+1,j+1,k2)) &
-  +rt2*(c1*w2(i,j,k2)  +c2*w2(i+1,j,k2) &
-  +c3*w2(i,j+1,k2)+c4*w2(i+1,j+1,k2))
+      +c3*w1(i,j+1,k2)+c4*w1(i+1,j+1,k2)) &
+      +rt2*(c1*w2(i,j,k2)  +c2*w2(i+1,j,k2) &
+      +c3*w2(i,j+1,k2)+c4*w2(i+1,j+1,k2))
   w=wk1*dz1+wk2*dz2
 
   m=icomp(np)
 
   if(kgravity(m) > 0) then
-  
+
   !..potential temperature (no pot.temp. at surface...)
     kt1= max(k1,2)
     kt2= kt1+1
     tk1= rt1*(c1*t1(i,j,  kt1)+c2*t1(i+1,j,  kt1) &
-    +c3*t1(i,j+1,kt1)+c4*t1(i+1,j+1,kt1)) &
-    +rt2*(c1*t2(i,j,  kt1)+c2*t2(i+1,j,  kt1) &
-    +c3*t2(i,j+1,kt1)+c4*t2(i+1,j+1,kt1))
+        +c3*t1(i,j+1,kt1)+c4*t1(i+1,j+1,kt1)) &
+        +rt2*(c1*t2(i,j,  kt1)+c2*t2(i+1,j,  kt1) &
+        +c3*t2(i,j+1,kt1)+c4*t2(i+1,j+1,kt1))
     tk2= rt1*(c1*t1(i,j,  kt2)+c2*t1(i+1,j,  kt2) &
-    +c3*t1(i,j+1,kt2)+c4*t1(i+1,j+1,kt2)) &
-    +rt2*(c1*t2(i,j,  kt2)+c2*t2(i+1,j,  kt2) &
-    +c3*t2(i,j+1,kt2)+c4*t2(i+1,j+1,kt2))
+        +c3*t1(i,j+1,kt2)+c4*t1(i+1,j+1,kt2)) &
+        +rt2*(c1*t2(i,j,  kt2)+c2*t2(i+1,j,  kt2) &
+        +c3*t2(i,j+1,kt2)+c4*t2(i+1,j+1,kt2))
     th=tk1*dz1+tk2*dz2
-  
+
   !..pressure
     ps= rt1*(c1*ps1(i,j)  +c2*ps1(i+1,j) &
-    +c3*ps1(i,j+1)+c4*ps1(i+1,j+1)) &
-    +rt2*(c1*ps2(i,j)  +c2*ps2(i+1,j) &
-    +c3*ps2(i,j+1)+c4*ps2(i+1,j+1))
-  
+        +c3*ps1(i,j+1)+c4*ps1(i+1,j+1)) &
+        +rt2*(c1*ps2(i,j)  +c2*ps2(i+1,j) &
+        +c3*ps2(i,j+1)+c4*ps2(i+1,j+1))
+
     if(kgravity(m) == 2) then
       p= alevel(k1) + blevel(k1)*ps
       rtab=p*pmult
@@ -299,12 +309,12 @@ subroutine forwrd_dx(tf1,tf2,tnow,tstep,np,nparticle, &
       if(it >= size(vgtable,1)) it=size(vgtable,1)-1
       tvg= tbasevg + it*tincrvg
       grav1= vgtable(it,ip,m) &
-      + (vgtable(it+1,ip,m)-vgtable(it,ip,m)) &
-      *(t-tvg)/tincrvg
+          + (vgtable(it+1,ip,m)-vgtable(it,ip,m)) &
+          *(t-tvg)/tincrvg
       ip= ip+1
       grav2= vgtable(it,ip,m) &
-      + (vgtable(it+1,ip,m)-vgtable(it,ip,m)) &
-      *(t-tvg)/tincrvg
+          + (vgtable(it+1,ip,m)-vgtable(it,ip,m)) &
+          *(t-tvg)/tincrvg
       gravity= grav1 + (grav2-grav1) * (p-pvg)/pincrvg
     !######################################################################
     !     if(np.lt.21) write(error_unit,*) '  p,t,gravity: ',p,t,gravity
@@ -312,10 +322,10 @@ subroutine forwrd_dx(tf1,tf2,tnow,tstep,np,nparticle, &
     else
       gravity= gravityms(m)
     end if
-  
+
     pdata(np)%grv= gravity
 
-  
+
   !..gravity ... a very simple, probably too simple (!!!) conversion
   !............. from m/s to model etadot/sigmadot "vertical velocity"
     k1=ivlayer(ilvl)
@@ -348,8 +358,6 @@ subroutine forwrd_dx(tf1,tf2,tnow,tstep,np,nparticle, &
   delx=u*dt
   dely=v*dt
   delz=w*dt
-
-!      end do
 
   return
 end subroutine forwrd_dx
