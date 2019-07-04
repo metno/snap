fbb12045cd8041ac5cad17a732dd6d768824a0f7
Magnus Ulimoen <magnusu@met.no>
2019-07-04 15:57:28 +0200

    rewrite rwalk
    rwalk had a parametrisation that was not consistent with
    the documentation. This was changed, with no penalty to
    correctness or performance

--- src/common/rwalk.f90
+++ src/common/rwalk.f90
@@ -16,18 +16,42 @@
 ! along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 module rwalkML
+  USE iso_fortran_env, only: real64
+
   implicit none
   private
 
-  public rwalk
+  real(real64), save :: vrdbla ! l-eta above mixing height
+  real(real64), save :: tfactor ! tfactor=tstep/tmix
+  real(real64), save :: tstep
+
+  real(real64), parameter :: hmax = 2500.0 ! maximum mixing height
+  real(real64), parameter :: tmix = 15.0*60.0 ! Characteristic mixing time = 15 min
+  real(real64), parameter :: lmax = 0.28 ! Maximum l-eta in the mixing layer
+  real(real64), parameter :: labove = 0.03 ! Standard l-eta above the mixing layer
+
+  public rwalk, rwalk_init
 
   contains
+
+!> Initialise constants needed for rwalk
+subroutine rwalk_init(timestep)
+!> time step in seconds (trajectory calculations)
+  real, intent(in) :: timestep
+
+  tfactor = timestep/tmix
+  tstep = timestep
+
+  ! l-eta above mixing height
+  vrdbla = labove*tfactor
+end subroutine
 
 !>  Purpose:  Diffusion, in and above boudary layer.
 !>
 !>  Method:   Random walk.
-subroutine rwalk(tstep,blfullmix,np,pextra)
-!---------------------------------------------------------------------
+!>
+!> ::rwalk_init must be run before rwalk
+subroutine rwalk(blfullmix,part,pextra)
 !   24.04.2009 Jerzy Bartnicki: Model particle which goes below the
 !   ground or above the top boundary in the random walk is reflected
 !   26.03.2011 Jerzy Bartnicki: New parameterization of vertical diffusion in the
@@ -39,195 +63,68 @@ subroutine rwalk(tstep,blfullmix,np,pextra)
 !   In ABL: l-eta=0.28*(mh/2500m)*(tstep/tstep-mix)
 !   Above ABL: l-eta=0.003*(tstep/tstep-mix)
 !   Entrainment zone = 10%*h
-  USE iso_fortran_env, only: real64
-  USE particleML
-  USE snapgrdML
-  USE snapparML
+  USE particleML, only: extraParticle, Particle
+!> full mixing in boundarylayer (true=old,false=new)
+  logical, intent(in) :: blfullmix
+!> particle with information
+  type(Particle), intent(inout)  :: part
+!> extra information regarding the particle (u, v, rmx, rmy)
+  type(extraParticle), intent(in) :: pextra
 
-  implicit none
-
-  REAL, INTENT(IN) :: tstep !< time step in seconds (trajectory calculations)
-  LOGICAL, INTENT(IN) :: blfullmix !< full mixing in boundarylayer (true=old,false=new)
-! particle-loop index, np = 0 means init
-  INTEGER, INTENT(IN)  :: np
-  TYPE(extraParticle), INTENT(IN) :: pextra
-
-
-  real(real64), save ::    a,cona,conb,vrange,vrdbla,vrdblb,vrqrt
-  real(real64), save :: hmax ! maximum mixing height = 2500m
-  real(real64), save :: tmix ! Characteristic mixing time = 15 min
-  real(real64), save :: lmax ! Maximum l-eta in the mixing layer = 0.28
-  real(real64), save :: labove ! Standard l-eta above the mixing layer
-  real(real64), save :: tfactor ! tfactor=tstep/tmix
-
-  real(real64) :: rnd(3), xrand, yrand, zrand, u, v, rl, vabs
+  real(real64) :: rnd(3), xrand, yrand, zrand, rl, vabs
   real(real64) :: hfactor, rv, rvmax
 
-
-! initialization
-  if (np == 0) then
-    hmax = 2500.0
-    tmix = 15.0*60.0
-    lmax = 0.28
-    labove=0.03
-    tfactor=tstep/tmix
-  !	write(*,*) 'hmax, tmix,lmax, labove, tfactor',
-  !     &hmax, tmix,lmax, labove, tfactor
-
-    a=0.5
-    conb=2.*tstep*0.5*(tstep**0.75)*(a**2)
-    a=0.25
-    cona=2.*tstep*0.5*(tstep**0.75)*(a**2)
-
-    vrange=0.02
-  !       vrdbla=vrange*0.5
-  ! l-eta above mixing height
-    vrdbla=labove*tfactor
-
-  ! l-eta below mixing height
-    vrdblb=lmax*tfactor
-    vrqrt=vrange*0.25
-    return
-  end if
-
-!--------------------------------------
-  if (blfullmix) then
-  !--------------------------------------
-
-  !      do np=1,npart // moved particle loop out
-
-  !..the rand function returns random real numbers between 0.0 and 1.0
-
-    call random_number(rnd)
-    xrand = rnd(1) - 0.5
-    yrand = rnd(2) - 0.5
-    zrand = rnd(3) - 0.5
+  real(real64) :: a
+  real(real64), parameter :: b = 0.875
 
 
-    if(pdata(np)%z > pdata(np)%tbl) then
+! the rand function returns random real numbers between 0.0 and 1.0
+  call random_number(rnd)
+  xrand = rnd(1) - 0.5
+  yrand = rnd(2) - 0.5
+  zrand = rnd(3) - 0.5
 
-    !..particle in boundary layer.....................................
+! horizontal diffusion
+  if (part%z > part%tbl) then ! in boundary layer
+    a = 0.5
+  else ! above boundary layer
+    a = 0.25
+  endif
 
-    !..horizontal diffusion
-      u=pextra%u
-      v=pextra%v
-      vabs=sqrt(u*u+v*v)
-      rl=sqrt(conb*vabs**1.75)*2.
-      pdata(np)%x=pdata(np)%x+rl*xrand*pextra%rmx
-      pdata(np)%y=pdata(np)%y+rl*yrand*pextra%rmy
+  vabs = hypot(pextra%u, pextra%v)
+  rl = 2*a*((vabs*tstep)**b)
+  part%x = part%x + rl*xrand*pextra%rmx
+  part%y = part%y + rl*yrand*pextra%rmy
 
-    !..vertical diffusion
 
-      pdata(np)%z=1.-(1.-pdata(np)%tbl)*1.1*(zrand+0.5)
+! vertical diffusion
+  if (part%z <= part%tbl) then ! Above boundary layer
+      part%z = part%z + vrdbla*zrand
+  else ! In boundary layer
+    if (blfullmix) then
+      part%z = 1.0 - (1.0 - part%tbl)*1.1*(zrand+0.5)
+    else ! not full mixing
+      hfactor = part%hbl/hmax
+      rv = lmax*hfactor*tfactor
+      rvmax = 1.0 - part%tbl
 
-    else
-
-    !..particle above boundary layer..................................
-
-    !..horizontal diffusion
-      u=pextra%u
-      v=pextra%v
-      vabs=sqrt(u*u+v*v)
-      rl=sqrt(cona*vabs**1.75)*2.
-      pdata(np)%x=pdata(np)%x+rl*xrand*pextra%rmx
-      pdata(np)%y=pdata(np)%y+rl*yrand*pextra%rmy
-
-    !..vertical diffusion
-      pdata(np)%z=pdata(np)%z+vrdbla*zrand
-
-    end if
-
-  !      end do
-
-  !--------------------------------------
-  else
-  !--------------------------------------
-
-  !... no full mixing
-
-  !      do np=1,npart  // moved particle loop out
-  !	write(*,*) np,(pdata(i,np),i=1,5)
-
-  !..the rand function returns random real numbers between 0.0 and 1.0
-
-    call random_number(rnd)
-    xrand = rnd(1) - 0.5
-    yrand = rnd(2) - 0.5
-    zrand = rnd(3) - 0.5
-
-    if(pdata(np)%z > pdata(np)%tbl) then
-
-    !..particle in boundary layer.....................................
-
-    !..horizontal diffusion
-      u=pextra%u
-      v=pextra%v
-      vabs=sqrt(u*u+v*v)
-      rl=sqrt(conb*vabs**1.75)*2.
-      pdata(np)%x=pdata(np)%x+rl*xrand*pextra%rmx
-      pdata(np)%y=pdata(np)%y+rl*yrand*pextra%rmy
-
-    !..vertical diffusion
-      hfactor=pdata(np)%hbl/hmax
-      rv=lmax*hfactor*tfactor
-      rvmax=1.0-pdata(np)%tbl
-    !	write(*,*) 'hfactor,rv,rvmax,pdata(np)%tbl,pdata(np)%hbl',
-    !     &hfactor,rv,rvmax,pdata(np)%tbl,pdata(np)%hbl
-    !	if(rv .gt. rvmax) then
-    !	write(*,*) 'rv,rvmax,pdata(np)%z,pdata(np)%tbl',
-    !     &  rv,rvmax,pdata(np)%z,pdata(np)%tbl
-    !	stop
-    !	endif
-
-      if(rv > rvmax) rv=rvmax
-      pdata(np)%z=pdata(np)%z+rv*zrand
+      rv = min(rv, rvmax)
+      part%z = part%z + rv*zrand
 
     !... reflection from the ABL top
-
-      if(pdata(np)%z < pdata(np)%tbl) then
-      !	  write(*,*) 'gora-przed, p4=',pdata(np)%tbl
-      !	  write(*,*) 'gora-przed, p3=',pdata(np)%z
-        pdata(np)%z= 2.0*pdata(np)%tbl-pdata(np)%z
-      !	  write(*,*) 'gora-po   , p3=',pdata(np)%z
+      if(part%z < part%tbl) then
+        part%z = 2.0*part%tbl - part%z
       endif
 
     !... reflection from the bottom
-
-      if(pdata(np)%z > 1.0) then
-      !	  write(*,*) 'dol-przed, p=',pdata(np)%z
-        pdata(np)%z= 2.0-pdata(np)%z
-      !	  write(*,*) 'dol-po   , p=',pdata(np)%z
+      if(part%z > 1.0) then
+        part%z = 2.0 - part%z
       endif
 
     !..vertical limits
-      if(pdata(np)%z > 1.0) pdata(np)%z=1.0
-      if(pdata(np)%z < pdata(np)%tbl) &
-      pdata(np)%z=pdata(np)%tbl
-
-    else
-
-    !..particle above boundary layer..................................
-
-    !..horizontal diffusion
-      u=pextra%u
-      v=pextra%v
-      vabs=sqrt(u*u+v*v)
-      rl=sqrt(cona*vabs**1.75)*2.
-
-      pdata(np)%x=pdata(np)%x+rl*xrand*pextra%rmx
-      pdata(np)%y=pdata(np)%y+rl*yrand*pextra%rmy
-
-    !..vertical diffusion
-      pdata(np)%z=pdata(np)%z+vrdbla*zrand
-
+      part%z = min(part%z, 1.0d0)
+      part%z = max(part%z, real(part%tbl, kind=kind(part%z)))
     end if
-
-  !      end do
-
-  !--------------------------------------
   end if
-!--------------------------------------
-
-  return
 end subroutine rwalk
 end module rwalkML
