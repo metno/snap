18fa252813128aba2b1a0e7bece91aa573328416
Magnus Ulimoen <magnusu@met.no>
2019-07-18 11:30:53 +0200

    lint epinterp

--- src/common/epinterp.f90
+++ src/common/epinterp.f90
@@ -23,38 +23,33 @@ module epinterpML
 
   contains
 
+!> simple bilinear interpolation
 subroutine epinterp(nxf,nyf,field,npos,xpos,ypos,zpos,inside)
+  integer, intent(in) :: nxf,nyf,npos
+  real, intent(in) :: field(nxf,nyf)
+  real, intent(in) :: xpos(npos),ypos(npos)
+  real, intent(out) :: zpos(npos)
+  integer, intent(in) :: inside(npos)
 
-!  simple bilinear interpolation
+  real, parameter :: undef = +1.0e+35
 
-  implicit none
-
-!..input
-  integer :: nxf,nyf,npos
-  real ::    field(nxf,nyf)
-  real ::    xpos(npos),ypos(npos),zpos(npos)
-  integer :: inside(npos)
-
-!..local
   integer :: n,i,j
-  real ::    undef,dx,dy,c1,c2,c3,c4
-
-  undef=+1.e+35
+  real :: dx,dy,c1,c2,c3,c4
 
   do n=1,npos
     if(inside(n) == 1) then
-      i=xpos(n)
-      j=ypos(n)
-      dx=xpos(n)-i
-      dy=ypos(n)-j
-      c1=(1.-dy)*(1.-dx)
-      c2=(1.-dy)*dx
-      c3=dy*(1.-dx)
-      c4=dy*dx
-      zpos(n)= c1*field(i,j)  +c2*field(i+1,j) &
-      +c3*field(i,j+1)+c4*field(i+1,j+1)
+      i = int(xpos(n))
+      j = int(ypos(n))
+      dx = xpos(n) - i
+      dy = ypos(n) - j
+      c1 = (1.0 - dy)*(1.0 - dx)
+      c2 = (1.0 - dy)*dx
+      c3 = dy*(1.0 - dx)
+      c4 = dy*dx
+      zpos(n) = c1*field(i,j) + c2*field(i+1,j) &
+          + c3*field(i,j+1) + c4*field(i+1,j+1)
     else
-      zpos(n)=undef
+      zpos(n) = undef
     end if
   end do
 
