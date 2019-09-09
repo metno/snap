66ef0846ae4e6d8a469feb5ace6a3781f98ff171
Magnus Ulimoen <magnusu@met.no>
2019-09-09 11:07:26 +0000

    add makefile for debug build

---
+++ src/debug.mk
@@ -0,0 +1,14 @@
+# link this to current.mk and it will be used in the Makefiles in subdirectories
+# includefile contains Compiler definitions etc.
+
+F77 = gfortran
+
+F77FLAGS=-DVERSION=\"$(VERSION)\" -Og -g -std=gnu -Wall -Wextra -fimplicit-none -fmodule-private -Wuse-without-only -fcheck=all
+MILIB_FLAGS = -fno-implicit-none -fno-module-private -Wno-all -Wno-extra
+
+BINDIR=../../bin/
+INCLUDES=$(shell nf-config --fflags)
+
+BLIBS= $(shell nf-config --flibs)
+
+.SUFFIXES:
