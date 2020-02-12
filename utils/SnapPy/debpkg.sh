5b3b993c4e5b3514c59555f2f0913cffd0ef27e6
André Brodtkorb <andre.brodtkorb@met.no>
2020-02-12 10:22:19 +0100

    Updated to version 1.6.11

--- utils/SnapPy/debpkg.sh
+++ utils/SnapPy/debpkg.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-cat $0 | sed -n '/INSTRUCTIONS$/,/INSTRUCTIONS_END$/p'
+if [[ "$1" != "-y" ]]; then
+    cat $0 | sed -n '/INSTRUCTIONS$/,/INSTRUCTIONS_END$/p'
+    exit 0
+fi
 
 #INSTRUCTIONS
 # 1 - Update version, changelog, etc. in this script
+#     Then run this script with -y option
 # 2 - Upload to internal package repository using dupload 
 #     (you need a properly configured .dupload.conf in your home directory, something like)
 #
@@ -20,27 +24,31 @@ cat $0 | sed -n '/INSTRUCTIONS$/,/INSTRUCTIONS_END$/p'
 #      };
 #
 #     Check first that it looks plausible:
-#     $ dupload --no --to xenialold dist/snap-py_1.6.10-1_amd64.changes
+#     $ dupload --no --to xenialold dist/snap-py_<version>_amd64.changes
 #     then remove --no option
 # 3 - Check that it works on one machine
+#     $ ssh -X vglserver2
+#     $ sudo apt-get update
+#     $ sudo apt-get install snap-py
+#     $ snapPy
+#     If something goes wrong, downgrade to last version again using 
+#     $ sudo apt-get install snap-py=<version-number>
+# 4 - Roll out to all machines with ansible
 #     # Setup
 #     $ git clone git@gitlab.met.no:ansible/workstation.git
 #     $ sudo apt-get install ansible
 #     # Rollout
 #     $ cd workstation/vgl
 #     #IMPORTANT: snap-py is in group misc - only reinstall the misc packages
+#     #Roll out to e.g. vglserver2
 #     $ ansible-playbook -i hosts -t misc -l vglserver2 install.yml
-#     $ ssh -X vglserver2 snap-py
-#     If something goes wrong, downgrade to last version again using 
-#     $ ssh vglserver2
-#     $ sudo apt-get install snap-py=<version-number>
-# 4 - Roll out to all machines with ansible
+#     #Roll out to all hosts
 #     $ ansible-playbook -i hosts -t misc install.yml
 #INSTRUCTIONS_END
 
 
-VERSION=1.6.10
-CHANGELOG="Better handling of multiple users and emission altitude"
+VERSION=1.6.11
+CHANGELOG="Fix race condition volcano.xml vs volcano.log"
 dch -v ${VERSION}-1 -U "${CHANGELOG}"
 dch -r ''
 VERSION=$VERSION python3 setup.py sdist
