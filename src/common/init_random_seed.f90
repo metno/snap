b310ed5048f8641121c04cbed44bb7af5748ef63
Magnus Ulimoen <magnusu@met.no>
2020-02-25 10:22:04 +0100

    prevent realloc code in random_seed

--- src/common/init_random_seed.f90
+++ src/common/init_random_seed.f90
@@ -30,7 +30,7 @@ module init_random_seedML
   CALL RANDOM_SEED(size = n)
   ALLOCATE(seed(n))
 
-  seed = 3 + 37 * (/ (i - 1, i = 1, n) /)
+  seed(:) = 3 + 37 * (/ (i - 1, i = 1, n) /)
   CALL RANDOM_SEED(PUT = seed)
 
 !  DEALLOCATE(seed), not called unsure if this doesn't break later calls to random-generator
