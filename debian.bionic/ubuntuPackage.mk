c11ce7541c26aaab8bc53f27f37f7c2c43448a08
Heiko Klein <heiko.klein@met.no>
2020-03-31 18:00:46 +0200

    fixing building on bionic

--- debian.bionic/ubuntuPackage.mk
+++ debian.bionic/ubuntuPackage.mk
@@ -5,26 +5,40 @@ F77 = gfortran
 CXX = g++
 CC  = gcc
 
-F77FLAGS=-O2 -g -mavx -cpp -ftree-vectorize -fno-math-errno
-CXXFLAGS=-O2 -mavx -ftree-vectorize -fno-math-errno
-CCFLAGS=-O2 -g -mavx -ftree-vectorize -fno-math-errno
+# PROFILES not working for gcc 5.4 and fortran MODULES
+#PROFILE=-fprofile-generate=/tmp/snapprof.dat
+#PROFILE=-fprofile-use=/tmp/snapprof.dat -Wcoverage-mismatch
+#
+#PROFILE=-flto # this is a few % slower with gcc 5.4
+#F77FLAGS=-O2 -g -mavx -ffpe-trap=invalid,zero,overflow -fbounds-check -Wall
+#-DPETTERSEN
+# -ffpe-trap=invalid,zero,overflow no longer usable, conflict/bug in gfortran with IEEE_VALUE(IEEE_QUIET_NAN)
+F77FLAGS=-DVERSION=\"$(VERSION)\" -O2 -ftree-vectorize -fno-math-errno -g -mavx -fopt-info-optimized-vec -fopenmp $(PROFILE) -Wall -Wextra -fimplicit-none -fmodule-private -std=gnu -Wuse-without-only
+CXXFLAGS=-O3 $(PROFILE) -Wall -Wextra
+CCFLAGS=-O3 -g $(PROFILE) -Wall -Wextra
 
-LDFLAGS=
+LDFLAGS=-fopenmp $(PROFILE)
 
 # NCDIR required even if /usr or /usr/local
 NCDIR = /usr
 
+# optional fimex
+# pkg-config --cflags fimex
+# pkg-config --libs fimex
+FIMEXINC = -I/usr/include/fimex-1.4
+FIMEXLIB =  -lfimexf-1.4 -lfimex-1.4
 
 MIINC = -I/usr/include
+MILIB_FLAGS = -fno-implicit-none -fno-module-private -Wno-all -Wno-extra
 # uncomment MILIB if felt-support not required
-MILIB = -L/usr/lib -lmi
+#MILIB = -L/usr/lib -lmi
 EXLIBS = -lpthread -ldl
 
 ##########################################################
 
 BINDIR=../debian/bsnap/usr/bin/
 
-INCLUDES = -I. $(MIINC)
+INCLUDES = -I. $(MIINC) $(FIMEXINC)
 
 
 ifdef NCDIR
@@ -33,8 +47,8 @@ INCLUDES += -I$(NCDIR)/include
 endif
 
 LIBS= $(MILIB) $(EXLIBS)
-BLIBS += $(MILIB) -lnetcdff
+BLIBS += $(MILIB) -lnetcdff $(FIMEXLIB)
+
 
 # clear out all suffixes
 .SUFFIXES:
-
