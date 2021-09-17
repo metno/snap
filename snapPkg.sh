59958aec6d00d705732af622f0c0077a6ccbe7f5
Magnus Ulimoen <magnusu@met.no>
2021-09-17 09:44:28 +0200

    Release new version of bsnap

--- snapPkg.sh
+++ snapPkg.sh
@@ -1,5 +1,6 @@
-VERSION=2.0.0
+VERSION=2.0.4
 VERSION_=`echo -n $VERSION | tr '.' '_'`
+export VERSION
 PLATFORM=bionic
 #svn copy https://svn.met.no/snap/trunk \
 #     https://svn.met.no/snap/tags/$VERSION -m "Release $VERSION" || exit 1
@@ -9,7 +10,7 @@ PLATFORM=bionic
 #git checkout master
 #git merge version${VERSION_}
 cd src
-VERSION=$VERSION make dist
+make dist
 cd ..
 tar xvfz snap-${VERSION}.tgz
 mv snap-${VERSION}.tgz bsnap_${VERSION}.orig.tar.gz
@@ -18,6 +19,6 @@ cp -r ../debian.${PLATFORM} .
 mv debian.${PLATFORM} debian
 dch -v ${VERSION}-1 "new release"
 dch -r ""
-debuild -us -uc -sa
+debuild --preserve-envvar VERSION -us -uc -sa
 
 
