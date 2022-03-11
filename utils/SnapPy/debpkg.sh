fcb911d17178dda8c359e38f743ec155f77ff481
Charlie Negri <charlie.negri@met.no>
2022-03-11 10:08:13 +0100

    Fix version number

--- utils/SnapPy/debpkg.sh
+++ utils/SnapPy/debpkg.sh
@@ -17,7 +17,7 @@ fi
 #     (you need a properly configured .dupload.conf in your home directory, copy this from https://gitlab.met.no/it/sd/klient/linux/internrepo/-/raw/master/.dupload.conf)
 #
 #     Check first that it looks plausible:
-#     $ dupload --no --to bionic dist/snap-py_<version>-2_amd64.changes
+#     $ dupload --no --to bionic dist/snap-py_<version>-1_amd64.changes
 #     then remove --no option
 #
 #     Wait for confirmation email that package has been accepted
@@ -52,7 +52,7 @@ VERSION=1.6.32
 CHANGELOG="parallelizing snap; adding options for input/output for manual runs"
 rm -f debian
 ln -s debian.$HOST debian
-dch -v ${VERSION}-2 -U "${CHANGELOG}"
+dch -v ${VERSION}-1 -U "${CHANGELOG}"
 dch -r ''
 VERSION=$VERSION python3 setup.py sdist
 cd dist || exit 1
