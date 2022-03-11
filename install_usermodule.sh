dee1a0bdefafa24c91c2c4562539a261ec61bee4
Magnus Ulimoen <magnusu@met.no>
2022-03-11 11:57:10 +0100

    Tweak prefix

--- install_usermodule.sh
+++ install_usermodule.sh
@@ -9,10 +9,14 @@
 VERSION=2.0.8
 export VERSION
 
+PREFIX=/modules/bionic/user-apps/SnapPy/"$VERSION"/
+export PREFIX
+
 cd utils/SnapPy || exit 2
-python3 setup.py install --prefix=/modules/bionic/user-apps/SnapPy/"$VERSION"/
+python3 setup.py install --prefix=$PREFIX
 cd ../.. || exit 2
 
 cd src || exit 2
+export BINDIR=$PREFIX/BIN
 ln -sf ubuntuLustreSnappy.mk current.mk
 make install
