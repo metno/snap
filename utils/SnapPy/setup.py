13cafa10555ea54051d6e69d9468255dd65c6d35
Magnus Ulimoen <magnusu@met.no>
2022-03-16 11:06:19 +0100

    Set execute bit and add eemepModelRunner to module

--- utils/SnapPy/setup.py
+++ utils/SnapPy/setup.py
@@ -2,18 +2,18 @@
 #
 # SNAP: Servere Nuclear Accident Programme
 # Copyright (C) 1992-2017   Norwegian Meteorological Institute
-# 
-# This file is part of SNAP. SNAP is free software: you can 
-# redistribute it and/or modify it under the terms of the 
-# GNU General Public License as published by the 
+#
+# This file is part of SNAP. SNAP is free software: you can
+# redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the
 # Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
@@ -21,19 +21,23 @@
 from distutils.core import setup
 import os
 
-version=os.getenv('VERSION', "0.5")
+version = os.getenv("VERSION", "0.5")
 
-setup(name='Snappy',
-      version=version,
-      description='SNAP GUI in python',
-      author='Heiko Klein',
-      author_email='Heiko.Klein@met.no',
-      url='https://gitlab.met.no/emep/snap',
-      packages=['Snappy', 'Snappy.EEMEP', 'METNO'],
-      package_dir={'Snappy': 'Snappy',
-                   'Snappy.EEMEP': 'Snappy/EEMEP'},
-      package_data={'Snappy': ['resources/*'],
-                    'Snappy.EEMEP': ['resources/*']},
-      scripts=['snapPy', 'snap4rimsterm', 'snapCombineInverse', 'snapRunnerNpps'],
+setup(
+    name="Snappy",
+    version=version,
+    description="SNAP GUI in python",
+    author="Heiko Klein",
+    author_email="Heiko.Klein@met.no",
+    url="https://gitlab.met.no/emep/snap",
+    packages=["Snappy", "Snappy.EEMEP", "METNO"],
+    package_dir={"Snappy": "Snappy", "Snappy.EEMEP": "Snappy/EEMEP"},
+    package_data={"Snappy": ["resources/*"], "Snappy.EEMEP": ["resources/*"]},
+    scripts=[
+        "snapPy",
+        "snap4rimsterm",
+        "snapCombineInverse",
+        "snapRunnerNpps",
+        "eemepModelRunner",
+    ],
 )
-
