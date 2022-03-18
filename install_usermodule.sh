1b70ba8f97c8c27a156e8e042a06d980646edeb3
Magnus Ulimoen <magnusu@met.no>
2022-03-18 09:54:27 +0100

    Usermodule installation tweaks

--- install_usermodule.sh
+++ install_usermodule.sh
@@ -3,13 +3,25 @@
 
 # module load netcdf-fortran/4.4.4
 # module load fimex
+set -e
 
 # This version must be the one used in SnapJobEC.py,
 # this might require an update to SnapJobEC.py!
-VERSION=2.0.8
+if [ -z "$1" ]; then
+    VERSION="TEST"
+else
+    VERSION="$1"
+fi
 export VERSION
 
 PREFIX=/modules/bionic/user-apps/SnapPy/"$VERSION"/
+if [ -d "$PREFIX" ]; then
+    echo "This version ('$VERSION') already exists, overwriting in 10 seconds"
+    echo "Use ctrl-C to cancel"
+    sleep 10
+else
+    mkdir "$PREFIX"
+fi
 export PREFIX
 
 cd utils/SnapPy || exit 2
@@ -18,10 +30,24 @@ if [ ! -f "Snappy/resources/1-s2.0-S0146645313000110-mmc1.zip" ]; then
     wget https://ars.els-cdn.com/content/image/1-s2.0-S0146645313000110-mmc1.zip --output-document Snappy/resources/1-s2.0-S0146645313000110-mmc1.zip
 fi
 
-python3 setup.py install --prefix=$PREFIX
+python3 setup.py install --prefix="$PREFIX"
 cd ../.. || exit 2
 
 cd src || exit 2
 export BINDIR=$PREFIX/bin/
 ln -sf ubuntuLustreSnappy.mk current.mk
+make clean
 make install
+
+cat > /modules/MET/bionic/user-modules/SnapPy/"$VERSION"<< EOF
+#%Module1.0#####################################################################
+##
+proc ModulesHelp { } {
+   puts stderr "\tSnapPy"
+}
+
+module-whatis   "SnapPy - scripts and python modules to run snap"
+set              root             /modules/bionic/user-apps/SnapPy/$VERSION
+prepend-path     PATH            \$root/bin
+prepend-path     PYTHONPATH      \$root/lib/python3.6/site-packages
+EOF
