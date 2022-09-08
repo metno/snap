59ac91b2a51680fe683c07349e69f05c6680b9a3
Heiko Klein <heiko.klein@met.no>
2022-09-08 14:27:15 +0000

    removing xenial rpath from bionic installation

--- src/ubuntuLustreBionicSnappy.mk
+++ src/ubuntuLustreBionicSnappy.mk
@@ -9,15 +9,18 @@ ifdef SNAP_DEBUG_CHECKS
   F77FLAGS+=$(F77DEBUGFLAGS)
 endif
 
+# optional fimex
+FIMEXLD = $(shell pkg-config --libs-only-L fimex  | cut -c 3-)
 FIMEXLIB = $(shell pkg-config --libs fimex)
 FIMEXINC =
 
+NCDIR=$(shell nf-config --prefix)
 NETCDFLIB = $(shell nf-config --flibs)
 NETCDFINC = $(shell nf-config --fflags)
 
 MILIB_FLAGS = -fno-implicit-none -fno-module-private -Wno-all -Wno-extra
 
-BINDIR=/modules/bionic/user-apps/SnapPy/$(VERSION)/bin/
+BINDIR ?= /modules/bionic/user-apps/SnapPy/$(VERSION)/bin/
 
 INCLUDES =
 
@@ -27,5 +30,17 @@ BLIBS += $(FIMEXLIB)
 INCLUDES += $(NETCDFINC)
 INCLUDES += $(FIMEXINC)
 
+ifdef NCDIR
+BLIBS += $(NCLIBS) -Wl,-rpath,$(NCDIR)/lib
+INCLUDES += $(NCINC)
+endif
+
+LIBS= $(EXLIBS)
+ifdef FIMEXLIB
+BLIBS += $(FIMEXLIB) -Wl,-rpath,$(FIMEXLD) $(LDFLAGS)
+endif
+
+
+# clear out all suffixes
 .SUFFIXES:
 
