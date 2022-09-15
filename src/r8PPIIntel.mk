f5c402687b37e06fe7213113826d8ff6a7479c58
Heiko Klein <heiko.klein@met.no>
2022-09-15 13:47:49 +0000

    adding fortran-intel makefile

---
+++ src/r8PPIIntel.mk
@@ -0,0 +1,46 @@
+# link this to current.mk and it will be used in the Makefiles in subdirectories
+# includefile contains Compiler definitions etc.
+
+# requires `module load netcdf-fortran-intel/4.6.0`
+
+F77 = ifort
+CXX = icc
+CC  = icc
+
+# OMP: -qopenmp
+# -heap-arrays
+F77FLAGS=-DVERSION=\"$(VERSION)\" -O2 -mavx -g -debug -warn all
+CXXFLAGS=-O2 -g
+CCFLAGS=-O2 -g
+
+LDFLAGS=-qopenmp
+
+# NCDIR not required if /usr or /usr/local
+NCDIR=$(shell nf-config --prefix)
+NCINC=$(shell nf-config --fflags)
+#NCLIBS=-L/modules/rhel8/user-apps/fou-modules/netcdf/netcdf_4.9.0/lib64 -L/modules/rhel8/user-apps/fou-modules/netcdff/netcdf-fortran_intel_4.6.0/lib64 -lnetcdff -lnetcdf -lnetcdff -Wl,-rpath,/modules/rhel8/user-apps/fou-modules/netcdf/netcdf_4.9.0/lib64 -Wl,-rpath,/modules/rhel8/user-apps/fou-modules/netcdff/netcdf-fortran_intel_4.6.0/lib64
+NCLIBS=$(shell nf-config --flibs)
+NCLIBS+=-Wl,-rpath,/modules/rhel8/user-apps/Compiler/Intel-2018/compilers_and_libraries/linux/lib/intel64/ -Wl,-rpath,/modules/rhel8/user-apps/fou-modules/netcdf/netcdf_4.9.0/lib64 -Wl,-rpath,/modules/rhel8/user-apps/fou-modules/netcdff/netcdf-fortran_intel_4.6.0/lib64 -Wl,-rpath,/modules/rhel8/user-apps/fou-modules/hdf5/hdf5_1_13_1/lib/
+
+
+EXLIBS = -lpthread -ldl
+
+##########################################################
+
+BINDIR=../../bin/
+
+INCLUDES = -I.
+
+
+ifdef NCDIR
+BLIBS += $(NCLIBS) -Wl,-rpath,$(NCDIR)/lib
+INCLUDES += $(NCINC)
+endif
+
+LIBS= $(MILIB) $(EXLIBS)
+BLIBS += $(MILIB) $(NCLIBS)
+
+
+# clear out all suffixes
+.SUFFIXES:
+
