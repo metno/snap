466463653d8bca89ac39cd9db517bae42911ed74
Heiko Klein <heiko.klein@met.no>
2023-06-19 12:33:48 +0200

    removing EC-global (0.1/slow) runs from vgl. Superceded by EC-global (0.2 long forecast)

--- utils/SnapPy/Snappy/EcMeteorologyCalculator.py
+++ utils/SnapPy/Snappy/EcMeteorologyCalculator.py
@@ -118,6 +118,8 @@ release=$(lsb_release --codename --short)
 if [[ "$release" == "Ootpa" ]]; then
     module use /modules/MET/rhel8/user-modules/ /modules/MET/rhel8/user-modules/fou-modules
     module load ecdis4cwf/1.6.1
+elsif [[ "$release" == "bionic" ]]; then
+    module load ecdis4cwf/1.2.6
 else
     echo "ERROR: unknown OS release $release, don't know how to load ecdis4cwf"
     exit 1
