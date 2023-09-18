e404cc1beb0ba562c5701be0282fa7f1e2070848
Heiko Klein <heiko.klein@met.no>
2023-09-18 10:49:00 +0000

    don't create components with 0 emissions

--- utils/SnapPy/Snappy/EEMEP/ColumnSouce2SNAPRelease.py
+++ utils/SnapPy/Snappy/EEMEP/ColumnSouce2SNAPRelease.py
@@ -6,7 +6,7 @@ import math
 # 7BIN VAAC distribution (Gravset_mod.f90), last is e.g. size 30-100, log-normal-distribution
 diameters = [0.1, 0.3, 1.0, 3.0, 10.0, 30.0, 100.0]
 distrib   = [0.0, 0.1, 0.5, 5.0, 20.0, 70.0,   4.4]
-classnames= [f"ASH_{i}" for i,_ in enumerate(diameters)]
+classnames= [(i, f"ASH_{i}") for i,_ in enumerate(diameters) if distrib[i] > 0]
 
 def readEmepWriteSnap(infile, outfile, outsnap):
     FIELDS = {
@@ -28,6 +28,8 @@ def readEmepWriteSnap(infile, outfile, outsnap):
                 datarow[x] = datetime.datetime.strptime(row[FIELDS[x]].strip(), "%Y-%m-%d %H:%M:%S")
             for x in ('dM/dt[kBq/s]','m63'):
                 datarow[x] = float(row[FIELDS[x]])
+            if row[FIELDS['BASE[km]']] == "VENT":
+                row[FIELDS['BASE[km]']] = 0
             for x in ('BASE[km]', 'H[km]'):
                 ox = x.replace('km','m')
                 datarow[ox] = round(float(row[FIELDS[x]])*1000)
@@ -57,7 +59,7 @@ def readEmepWriteSnap(infile, outfile, outsnap):
                     last_start = last_end
                     last_end = row['START']
                     for rl in release_lower:
-                        for cn in classnames:
+                        for (i, cn) in classnames:
                             ofh.write(f"{release_hour_steps[-1]} {rl} {cn} 0\n")
                 step = (row['START'] - last_start).total_seconds() / (60 * 60)
                 release_hour_steps.append(step + release_hour_steps[-1])
@@ -68,7 +70,7 @@ def readEmepWriteSnap(infile, outfile, outsnap):
                 release_lower.append(row['BASE[m]'])
             if not row['H[m]'] in release_upper:
                 release_upper.append(row['H[m]'])
-            for i, cn in enumerate(classnames):
+            for (i, cn) in classnames:
                 emis = row["dM/dt[kBq/s]"] * 1000 * distrib[i] / 100 # % -> frac; kBq -> Bq
                 ofh.write(f"{release_hour_steps[-1]} {row['BASE[m]']} {cn} {emis:.0f}\n")
 
@@ -78,9 +80,9 @@ def readEmepWriteSnap(infile, outfile, outsnap):
         ofh.write(f"RELEASE.HEIGHTLOWER.M = {', '.join([str(x) for x in release_lower])}\n")
         ofh.write(f"RELEASE.HEIGHTUPPER.M = {', '.join([str(x) for x in release_lower])}\n")
         ofh.write(f"RELEASE.HEIGHTRADIUS.M = {', '.join(['1000' for _ in release_lower])}\n")
-        comps = [f"'{x}'" for x in classnames]
+        comps = [f"'{x}'" for (i, x) in classnames]
         ofh.write(f"RELEASE.COMPONENTS= {', '.join(comps)}\n\n")
-        for i, cn in enumerate(classnames):
+        for (i, cn) in classnames:
             if i < 2:
                 radius = diameters[i] / 2
             else:
@@ -96,6 +98,12 @@ def readEmepWriteSnap(infile, outfile, outsnap):
             ofh.write("DENSITY.G/CM3=1\n\n") # rephra between 0.4 and 1.5
 
 if __name__ == "__main__":
-    readEmepWriteSnap("/lustre/storeB/project/fou/kl/eva/eemep/TestRuns_Eyja/eemep_Eyjafjoll_ondemand_20230413/columnsource_emission_eya.csv",
-                      "release.txt",
-                      "snap_eyja.input")
+    import os
+    if os.path.exists("./columnsource_emission.csv"):
+        readEmepWriteSnap("./columnsource_emission.csv",
+                        "release.txt",
+                        "snap.input")
+    else:
+        readEmepWriteSnap("/lustre/storeB/project/fou/kl/eva/eemep/TestRuns_Eyja/eemep_Eyjafjoll_ondemand_20230413/columnsource_emission_eya.csv",
+                        "release.txt",
+                        "snap_eyja.input")
