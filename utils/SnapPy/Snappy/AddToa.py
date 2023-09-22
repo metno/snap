1c5bda249f459ba2a046dfba4f3c14fab543cd33
Heiko Klein <heiko.klein@met.no>
2023-09-22 18:10:01 +0000

    adding SixHourMax to snapAddToa; adding column-conversion for ASH

--- utils/SnapPy/Snappy/AddToa.py
+++ utils/SnapPy/Snappy/AddToa.py
@@ -18,27 +18,31 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
+import logging
 import netCDF4
 import numpy
 
+from Snappy.EEMEP.SixHourMax import SixHourMax
 
-def add_toa_to_nc(nc: netCDF4.Dataset):
+def add_toa_to_nc(nc: netCDF4.Dataset, overwrite=False):
     """
     add time of arrival and total depositions
+    for ASH_ snap-files, creates also 6h-max and total-column
     to the nc-filehandle (netCDF4.Dataset)
     filehandle must be rewriteable snap-output file
     """
     if "time_of_arrival" in nc.variables:
-        return
+        if not overwrite:
+            return
     times = netCDF4.num2date(nc.variables["time"][:], units=nc.variables["time"].units)
     timeMax = abs(int((times[-1] - times[0]).total_seconds() / (60 * 60)))
     timeDelta = abs(int((times[1] - times[0]).total_seconds() / (60 * 60)))
     data = 0
-    exampleVar = ""
-    for v in list(nc.variables.keys()):
-        if v[-18:] == "_acc_concentration" or v[-19:] == "_acc_wet_deposition":
-            exampleVar = v
-            data += nc.variables[v][:]
+    exampleVar = None
+    for v, var in nc.variables.items():
+        if v.endswith("_acc_concentration") or v.endswith("_acc_wet_deposition"):
+            exampleVar = var
+            data += var[:]
     if not isinstance(data, numpy.ndarray):
         raise Exception(
             f"no variable with *_acc_concentration found in file: {nc.filepath()}"
@@ -58,31 +62,30 @@ def add_toa_to_nc(nc: netCDF4.Dataset):
     if "time_of_arrival" in nc.variables:
         # for debugging or in special cases only
         toaVar = nc["time_of_arrival"]
-        print("overwriting existing variable time_of_arrival")
+        logging.warning("overwriting existing variable time_of_arrival")
     else:
         toaVar = nc.createVariable(
             "time_of_arrival",
             "f",
-            nc.variables[exampleVar].dimensions,
+            exampleVar.dimensions,
             zlib=True,
             fill_value=-999,
         )
     toaVar.units = "hours"
     for attr in ("grid_mapping", "coordinates"):
-        if attr in nc.variables[exampleVar].ncattrs():
-            toaVar.setncattr(attr, nc.variables[exampleVar].getncattr(attr))
+        if attr in exampleVar.ncattrs():
+            toaVar.setncattr(attr, exampleVar.getncattr(attr))
     toaVar[0, :] = toa
 
     # add also total depositions
-    for v in list(nc.variables.keys()):
-        if v[-19:] == "_acc_wet_deposition":
-            comp = v[:-19]
-            #            print(comp)
-            total = (
-                nc.variables[v][:]
-                + nc.variables["{}_acc_dry_deposition".format(comp)][:]
-            )
-            totalVarName = "{}_acc_total_deposition".format(comp)
+    for v, var in nc.variables.items():
+        if v.endswith("_acc_dry_deposition"):
+            total = var[:]
+            comp = v.replace("_acc_dry_deposition","")
+            wdepName = f"{comp}_acc_dry_deposition"
+            if wdepName in nc.variables:
+                total += nc.variables[wdepName][:]
+            totalVarName = f"{comp}_acc_total_deposition"
             if totalVarName not in nc.variables:
                 totalVar = nc.createVariable(
                     totalVarName, "f", nc.variables[v].dimensions, zlib=True
@@ -92,14 +95,34 @@ def add_toa_to_nc(nc: netCDF4.Dataset):
                     if attr in nc.variables[v].ncattrs():
                         totalVar.setncattr(attr, nc.variables[v].getncattr(attr))
                 totalVar[:] = total
+    nc.sync()
+
+    # postprocess snap ash-files
+    if SixHourMax.detect_ash_model(nc) == 'snap':
+        SixHourMax(nc)
+
 
 
 if __name__ == "__main__":
     import argparse
 
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        description="""Add time_of_arrival, total_deposition to the nc-file.
+                       For ASH, it adds also MAX6h_ASH in flightlevels
+                       and COLUMN_ASH_kmax."""
+    )
     parser.add_argument("snapNc", help="snap.nc file to be changed")
+    parser.add_argument("--overwrite",
+                        help="force rewriting time_of_arrival, even if it exists",
+                        action="store_true")
+    parser.add_argument(
+        '-v', '--verbose',
+        help="Be verbose",
+        action="store_const", dest="loglevel",
+        const=logging.INFO,
+        default=logging.WARNING
+    )
     args = parser.parse_args()
-
+    logging.basicConfig(level=args.loglevel)
     with netCDF4.Dataset(args.snapNc, "a") as nc:
-        add_toa_to_nc(nc)
+        add_toa_to_nc(nc, args.overwrite)
