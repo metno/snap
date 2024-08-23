b46d3acb428c75b34c304351327401b8f32af18e
Heiko Klein <heiko.klein@met.no>
2024-08-23 15:03:40 +0200

    versioning

--- utils/SnapPy/debpkg.sh
+++ utils/SnapPy/debpkg.sh
@@ -65,8 +65,8 @@ if [ ! -f "Snappy/resources/1-s2.0-S0146645313000110-mmc1.zip" ]; then
 fi
 
 HOST=$(lsb_release --codename --short)
-export VERSION=2.4.2
-CHANGELOG="adding Natanz"
+export VERSION=2.4.3
+CHANGELOG="fixing non-continuous releases; adding column-output"
 export DEBEMAIL=${USER}@met.no
 rm --force debian
 ln --symbolic debian.$HOST debian
