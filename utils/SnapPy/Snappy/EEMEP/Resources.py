9020786632d60ecbe707d5d3dab93b5a319cbeec
Heiko Klein <heiko.klein@met.no>
2025-12-15 07:36:02 +0000

    allow up to 3 days of forecast

--- utils/SnapPy/Snappy/EEMEP/Resources.py
+++ utils/SnapPy/Snappy/EEMEP/Resources.py
@@ -302,7 +302,7 @@ class Resources(ResourcesCommon):
             for d in range(0, 1 + math.ceil(float(run_hours) / 24.0)):
                 startday = dtime + timedelta(days=d)
                 relevant_dates = []
-                for offset in range(0, 3):
+                for offset in range(0, 4):
                     # lowest offset is best, but earlier forecasts will do
                     curday = startday + timedelta(days=-1 * offset)
                     day_files = []
