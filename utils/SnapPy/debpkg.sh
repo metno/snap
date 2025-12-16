686c6520557855cc54fd5117716324ec53ea46db
Heiko Klein <heikok@met.no>
2025-12-16 16:02:22 +0000

    snappy-v2.5.12

--- utils/SnapPy/debpkg.sh
+++ utils/SnapPy/debpkg.sh
@@ -53,7 +53,7 @@ if [ ! -f "Snappy/resources/1-s2.0-S0146645313000110-mmc1.zip" ]; then
 fi
 
 HOST=$(lsb_release --codename --short)
-export VERSION=2.5.11
+export VERSION=2.5.12
 CHANGELOG="fix bug qt browser input parsing and handle better case with no release"
 export DEBEMAIL=${USER}@met.no
 rm --force debian
