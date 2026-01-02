08b23c3ae3f3a06a72007c3d7be87b78535d2aed
Halvard Sutterud <halvard.sutterud@gmail.com>
2026-01-02 14:16:57 +0000

    Conform to CF-1.4 conventions

--- utils/SnapPy/Snappy/resources/convert_largest_fraction_esa_to_snap.py
+++ utils/SnapPy/Snappy/resources/convert_largest_fraction_esa_to_snap.py
@@ -6,21 +6,27 @@ import xarray as xr
 import pandas as pd
 import pathlib
 
+import sys
 
-def add_attributes(largest_fraction, class_name="snap_class"):
+def add_attributes(largest_fraction):
     # Add attributes
+    largest_fraction.attrs['Conventions'] = 'CF-1.4'
+    largest_fraction["Main_Nature_Cover"].attrs['units'] = "1"
+    largest_fraction["Main_Nature_Cover"].attrs['long_name'] = "Nature cover with the largest fraction"
     comment = ("11: Sea, 12: Inland water, 13: Tundra/desert, 14: Ice and ice sheets, 15: Urban, 16: Crops, 17: Grass, "
                "18: Wetlands, 19: Evergreen needleleaf, 20: Deciduous broadleaf, 21: Mixed forest, 22: Shrubs and interrupted woodlands")
     largest_fraction["Main_Nature_Cover"].attrs['comment'] = comment
-    largest_fraction["Main_Nature_Cover"].attrs['long_name'] = "Nature cover with the largest fraction"
+    largest_fraction["Main_Nature_Cover"].attrs['coordinates'] = "lon lat"
+    largest_fraction["Main_Nature_Cover"].attrs['_FillValue'] = np.uint8(0)
 
-    largest_fraction = largest_fraction.drop_vars('time').drop_vars(class_name)
     return largest_fraction
 
 
 def convert_dtype(largest_fraction):
     # Convert to uint8 for extra space saving
     largest_fraction['Main_Nature_Cover'] = largest_fraction['Main_Nature_Cover'].astype(np.uint8)
+    largest_fraction['lat'] = largest_fraction['lat'].astype(np.float32)
+    largest_fraction['lon'] = largest_fraction['lon'].astype(np.float32)
     return largest_fraction
 
 
@@ -29,6 +35,7 @@ def calculate_largest_fraction(fractions, class_name='snap_class'):
 
     # Convert argmax indexes to class values
     largest_fraction['Main_Nature_Cover'] = fractions[class_name][largest_fraction['Main_Nature_Cover']]
+    largest_fraction = largest_fraction.drop_vars('time').drop_vars(class_name)
     return largest_fraction
 
 def convert_esa_to_snap(input_path, output_path, lookup_table='esa_to_snap.csv'):
@@ -58,7 +65,9 @@ def convert_esa_to_snap(input_path, output_path, lookup_table='esa_to_snap.csv')
     largest_fraction_snap = convert_dtype(largest_fraction_snap)
 
     # fractions_snap.to_netcdf('LandFractionEC.nc')
+    print(f"Saving data converted from {args.input_path} to {args.output_path}")
     largest_fraction_snap.to_netcdf(output_path)
+    print("Done")
 
 
 def get_args():
