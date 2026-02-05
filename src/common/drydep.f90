649cf17a846b1971bd2424e902980d1822f14105
George Cherry <george.cherry@met.no>
2026-02-05 09:09:21 +0000

    Formatting of drydep module

--- src/common/drydep.f90
+++ src/common/drydep.f90
@@ -1,19 +1,4 @@
-! SNAP: Servere Nuclear Accident Programme
-! Copyright (C) 1992-2017   Norwegian Meteorological Institute
-
-! This file is part of SNAP. SNAP is free software: you can
-! redistribute it and/or modify it under the terms of the
-! GNU General Public License as published by the
-! Free Software Foundation, either version 3 of the License, or
-! (at your option) any later version.
-
-! This program is distributed in the hope that it will be useful,
-! but WITHOUT ANY WARRANTY; without even the implied warranty of
-! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-! GNU General Public License for more details.
-
-! You should have received a copy of the GNU General Public License
-! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+!> Module for Dry deposition of radionuclides. Method: combination of J.Saltbones 1994 and J.Bartnicki 2003
 
 module drydepml
   use ISO_FORTRAN_ENV, only: real64, int8, int16
