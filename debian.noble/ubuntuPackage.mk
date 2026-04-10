db0010bb23095e0a94575fcf50305d67f5eb024f
Heiko Klein <heiko.klein@met.no>
2026-04-10 07:41:30 +0000

    use -march=x86-64-v3 rather than -mavx2.

--- debian.noble/ubuntuPackage.mk
+++ debian.noble/ubuntuPackage.mk
@@ -4,7 +4,7 @@
 F77 = gfortran
 
 F77DEBUGFLAGS= -ffpe-trap=invalid,zero,overflow -fbounds-check
-F77FLAGS=-DVERSION=\"$(VERSION)\" -O2 -ftree-vectorize -fno-math-errno -g -mavx2 -mfma -Wall -Wextra -fimplicit-none -fmodule-private -Wno-conversion
+F77FLAGS=-DVERSION=\"$(VERSION)\" -O2 -ftree-vectorize -fno-math-errno -g -march=x86-64-v3 -Wall -Wextra -fimplicit-none -fmodule-private -Wno-conversion
 ifdef SNAP_DEBUG_CHECKS
   F77FLAGS+=$(F77DEBUGFLAGS)
 endif
