c40b55cb697998cb7380f30464eed4b060d336dc
George Cherry <george.cherry@met.no>
2026-09-02 09:29:56 +0000

    Adds formatting to sourceterm

--- src/common/release.f90
+++ src/common/release.f90
@@ -508,6 +508,9 @@ subroutine release(istep,nsteph,tf1,tf2,tnow,ierror)
       write (sourceterm, 10) INT((istep+1)*tstep),",", &
       def_comp(n)%compname,",", hlower, ",", hupper, ",", run_comp(n)%totalbq
     end do
+
+    10 FORMAT(I7,A1,A6,A1,F8.3,A1,F8.3,A1,ES11.5)
+    
   ! c	write(error_unit,*) 'nparnum: ',nparnum
     write(iulog,*) 'nparnum: ',nparnum
   !      end if
