476584fd75df7d189a87802bc987d1ae549d2f11
George Cherry <george.cherry@met.no>
2026-09-02 11:29:47 +0000

    Fixes renaming of sourceterm io-unit

--- src/common/snapdebugML.F90
+++ src/common/snapdebugML.F90
@@ -29,7 +29,7 @@ module snapdebug
 !> output unit for log file
     integer, public :: iulog
 !> output unit for sourceterm file    
-    integer, public :: sourceterm
+    integer, public :: iu_sourceterm
 
     character(len=*), parameter :: global_timer_prefix = "timer: "
     character(len=*), parameter :: global_timer_total_prefix = "Total: "
