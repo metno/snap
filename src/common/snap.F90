0fd6d78328161a0effbb734588ce19dd45c8435f
George Cherry <113997294+jjordene@users.noreply.github.com>
2026-09-11 10:54:27 +0200

    Apply suggestion from @heikoklein
    Co-authored-by: Heiko Klein <Heiko.Klein@met.no>

--- src/common/snap.F90
+++ src/common/snap.F90
@@ -660,7 +660,7 @@ PROGRAM bsnap
     write (error_unit, *) "OpenMP: not enabled"
 #endif
     write (iu_sourceterm, '("Start time [seconds since ",I4,"-",I2.2,"-",I2.2,"T",I2.2,":00:00Z], &
-      & End time [seconds since ",I4,"-",I2.2,"-",I2.2,"T",I2.2,":00Z], &
+      & End time [seconds since ",I4,"-",I2.2,"-",I2.2,"T",I2.2,":00:00Z], &
       & Component name, &
       & Lower height [m], &
       & Upper height [m], &
