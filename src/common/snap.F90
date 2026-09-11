87bbc5b79cb92c0cf13d679a21b60290b65b56ad
George Cherry <george.cherry@met.no>
2026-09-11 07:09:06 +0000

    Fix headers

--- src/common/snap.F90
+++ src/common/snap.F90
@@ -659,8 +659,8 @@ PROGRAM bsnap
     write (iulog, *) "OpenMP: not enabled"
     write (error_unit, *) "OpenMP: not enabled"
 #endif
-    write (iu_sourceterm, '("Start timestep [seconds since ",I4,"-",I2.2,"-",I2.2,"T",I2.2,":00Z], &
-      & End timestep [seconds since ",I4,"-",I2.2,"-",I2.2,"T",I2.2,":00Z], &
+    write (iu_sourceterm, '("Start time [seconds since ",I4,"-",I2.2,"-",I2.2,"T",I2.2,":00Z], &
+      & End time [seconds since ",I4,"-",I2.2,"-",I2.2,"T",I2.2,":00Z], &
       & Component name, &
       & Lower height [m], &
       & Upper height [m], &
